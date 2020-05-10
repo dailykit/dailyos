211dbe814720033459376529511869ce5e85757a
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-05-10 13:22:39 +0530

    fix: listing

--- src/apps/online_store/views/Listings/ProductsListing/index.jsx
+++ src/apps/online_store/views/Listings/ProductsListing/index.jsx
@@ -27,7 +27,7 @@ const address = 'apps.online_store.views.listings.productslisting.'
 
 const ProductsListing = () => {
    const { t } = useTranslation()
-   const [view, setView] = React.useState('customizable')
+   const [view, setView] = React.useState('combo')
    const [tunnels, openTunnel, closeTunnel] = useTunnel(1)
 
    const options = [
@@ -68,7 +68,7 @@ const ProductsListing = () => {
             </StyledHeader>
             <RadioGroup
                options={options}
-               active={'customizable'}
+               active={'combo'}
                onChange={option => setView(option.id)}
             />
             <Spacer />
