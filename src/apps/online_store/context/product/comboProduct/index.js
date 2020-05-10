cc6f01ccf7da12909e6589a3c29520ef1b6e4199
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-05-10 14:23:15 +0530

    chore: removed unused code

--- src/apps/online_store/context/product/comboProduct/index.js
+++ src/apps/online_store/context/product/comboProduct/index.js
@@ -7,60 +7,10 @@ export const state = {
       productType: '',
       componentId: '',
    },
-   id: '',
-   name: '',
-   tags: [],
-   description: '',
-   components: [],
 }
 
 export const reducers = (state, { type, payload }) => {
    switch (type) {
-      case 'SEED': {
-         return {
-            ...state,
-            id: payload.id,
-            name: payload.name,
-            tags: payload.tags,
-            description: payload.description,
-            components: payload.components,
-         }
-      }
-      case 'NAME': {
-         return {
-            ...state,
-            name: payload.value,
-         }
-      }
-      case 'DESCRIPTION': {
-         return {
-            ...state,
-            description: payload.value,
-         }
-      }
-      case 'TAGS': {
-         return {
-            ...state,
-            tags: payload.value,
-         }
-      }
-      case 'COMPONENTS': {
-         return {
-            ...state,
-            components: [...state.components, ...payload.components],
-         }
-      }
-      case 'UPDATE_COMPONENT': {
-         const index = state.components.findIndex(
-            comp => comp.id === payload.updatedComponent.id
-         )
-         const updatedComponents = state.components
-         updatedComponents[index] = payload.updatedComponent
-         return {
-            ...state,
-            components: updatedComponents,
-         }
-      }
       case 'META': {
          return {
             ...state,
