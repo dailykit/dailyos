0fabf74d85482e88ed89eed7ec9c9cbebb5a9078
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-05-18 10:18:48 +0530

    fix: count on home screen

--- src/apps/recipe/views/Home/index.js
+++ src/apps/recipe/views/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DashboardTile } from '@dailykit/ui'
+import { useSubscription } from '@apollo/react-hooks'
 
 // State
 import { Context } from '../../context/tabs'
@@ -7,6 +8,8 @@ import { Context } from '../../context/tabs'
 import { StyledHome, StyledCardList } from './styled'
 
 import { useTranslation, Trans } from 'react-i18next'
+
+import { S_INGREDIENTS, S_RECIPES } from '../../graphql'
 
 const address = 'apps.recipe.views.home.'
 
@@ -16,19 +19,23 @@ const Home = () => {
    const addTab = (title, view) => {
       dispatch({ type: 'ADD_TAB', payload: { type: 'listings', title, view } })
    }
+
+   const { data: ingredientsData } = useSubscription(S_INGREDIENTS)
+   const { data: recipeData } = useSubscription(S_RECIPES)
+
    return (
       <StyledHome>
          <h1>{t(address.concat('recipe app'))}</h1>
          <StyledCardList>
             <DashboardTile
-               title={t(address.concat("recipes"))}
-               count="29"
+               title={t(address.concat('recipes'))}
+               count={recipeData?.simpleRecipes.length || 0}
                conf="All available"
                onClick={() => addTab('Recipes', 'recipes')}
             />
             <DashboardTile
-               title={t(address.concat("ingredients"))}
-               count="29"
+               title={t(address.concat('ingredients'))}
+               count={ingredientsData?.ingredients.length || 0}
                conf="All available"
                onClick={() => addTab('Ingredients', 'ingredients')}
             />
