854bdacacbe6a41300e633bde0914cd3cbbcf035
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-05-22 13:19:58 +0530

    feat: card table

--- src/apps/recipe/views/Forms/RecipeForm/components/RecipeCard/index.js
+++ src/apps/recipe/views/Forms/RecipeForm/components/RecipeCard/index.js
@@ -1,7 +1,72 @@
 import React from 'react'
+import { Text, Checkbox, ComboButton } from '@dailykit/ui'
+import { Container, Flex } from '../styled'
+import { StyledTable, Preview, Pill } from './styled'
+import { EyeIcon } from '../../../../../assets/icons'
 
-const RecipeCard = () => {
-   return <div>Recipe Card</div>
+const RecipeCard = ({ state, openTunnel }) => {
+   return (
+      <Container top="32" bottom="32">
+         <Text as="title">Select Recipe Card Template</Text>
+         <Container top="16">
+            <StyledTable>
+               <thead>
+                  <tr>
+                     <th></th>
+                     <th>Template name</th>
+                     <th>Template type</th>
+                     <th>Paper size</th>
+                     <th>Print type</th>
+                     <th></th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr>
+                     <td>
+                        <Checkbox
+                           checked={true}
+                           onChange={val => console.log(val)}
+                        />
+                     </td>
+                     <td>
+                        <Flex direction="column">
+                           <Text as="p">Moonlight</Text>
+                           <Preview>
+                              <img src="https://source.unsplash.com/200x150/?paper" />
+                           </Preview>
+                        </Flex>
+                     </td>
+                     <td>
+                        <Flex justify="flex-start">
+                           <Pill active>Colored</Pill>
+                           <Pill>Non-colored</Pill>
+                        </Flex>
+                     </td>
+                     <td>
+                        <Flex justify="flex-start">
+                           <Pill active>A4</Pill>
+                           <Pill>A3</Pill>
+                           <Pill>Letter</Pill>
+                        </Flex>
+                     </td>
+                     <td>
+                        <Flex justify="flex-start">
+                           <Pill active>Image</Pill>
+                           <Pill>PDF</Pill>
+                        </Flex>
+                     </td>
+                     <td>
+                        <ComboButton type="ghost" onClick={() => openTunnel(9)}>
+                           <EyeIcon />
+                           Preview
+                        </ComboButton>
+                     </td>
+                  </tr>
+               </tbody>
+            </StyledTable>
+         </Container>
+      </Container>
+   )
 }
 
 export default RecipeCard
