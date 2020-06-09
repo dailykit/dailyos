3f8aedfd79aa5df7a6016d1a2e08e177cee4dd26
siddhantk232 <siddhantk232@gmail.com>
2020-06-09 15:26:13 +0530

    change: uncommented AuthProvider

--- src/apps/inventory/index.jsx
+++ src/apps/inventory/index.jsx
@@ -5,9 +5,9 @@ import { AuthProvider } from './context'
 import App from './App'
 
 const Inventory = () => (
-   // <AuthProvider>
-   <App />
-   // </AuthProvider>
+   <AuthProvider>
+      <App />
+   </AuthProvider>
 )
 
 export default Inventory
