6b25045dac03c467ac6576a3194b15aa2620fc54
siddhantk232 <siddhantk232@gmail.com>
2020-06-11 16:01:39 +0530

    feat: new item listings change: bulkItems are now available by default

--- src/apps/inventory/App.jsx
+++ src/apps/inventory/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import 'react-tabulator/css/bootstrap/tabulator_bootstrap.min.css'
+import 'react-tabulator/lib/styles.css'
+
 import { Context, state as initialState, reducers } from './context/tabs'
 
 // Sections
