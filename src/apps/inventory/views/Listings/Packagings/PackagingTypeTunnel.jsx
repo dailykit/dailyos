54a1a7ec86caa576e41fb9bf913d184584a1653a
siddhantk232 <siddhantk232@gmail.com>
2020-06-15 15:48:51 +0530

    change: creating package in listings now

--- src/apps/inventory/views/Listings/Packagings/PackagingTypeTunnel.jsx
+++ src/apps/inventory/views/Listings/Packagings/PackagingTypeTunnel.jsx
@@ -1,16 +1,49 @@
-import { Text } from '@dailykit/ui'
+import { Text, Loader } from '@dailykit/ui'
 import React from 'react'
+import { useMutation } from '@apollo/react-hooks'
+import { toast } from 'react-toastify'
 
+import { randomSuffix } from '../../../../../shared/utils/index'
 import { Spacer, TunnelContainer, TunnelHeader } from '../../../components'
 import { Context } from '../../../context/tabs'
 import { SolidTile } from '../styled'
+import { CREATE_PACKAGING } from '../../../graphql'
 
 export default function WorkOrderTypeTunnel({ close }) {
    const { dispatch } = React.useContext(Context)
 
-   const addTab = (title, view) => {
-      dispatch({ type: 'ADD_TAB', payload: { type: 'forms', title, view } })
+   const addTab = (title, view, id) => {
+      dispatch({
+         type: 'ADD_TAB',
+         payload: { type: 'forms', title, view, id },
+      })
    }
+
+   const [createPackaging, { loading }] = useMutation(CREATE_PACKAGING, {
+      onError: error => {
+         console.log(error)
+         toast.error('Error! Please try again')
+      },
+      onCompleted: input => {
+         const { name, id } = input.createPackaging.returning[0]
+         addTab(name, 'createPackaging', id)
+      },
+   })
+
+   const createPackagingHandler = type => {
+      const name = `pack-${randomSuffix()}`
+      createPackaging({
+         variables: {
+            object: {
+               name,
+               type,
+            },
+         },
+      })
+   }
+
+   if (loading) return <Loader />
+
    return (
       <TunnelContainer>
          <TunnelHeader
@@ -24,41 +57,14 @@ export default function WorkOrderTypeTunnel({ close }) {
             nextAction="Save"
          />
          <Spacer />
-         <SolidTile
-            onClick={() => {
-               dispatch({
-                  type: 'SET_PACKAGING_ID',
-                  payload: '',
-               })
-
-               dispatch({
-                  type: 'SET_PACKAGING_TYPE',
-                  payload: 'SACHET_PACKAGE',
-               })
-
-               addTab('New Packaging', 'sachetPackaging')
-            }}
-         >
+         <SolidTile onClick={() => createPackagingHandler('SACHET_PACKAGE')}>
             <Text as="h1">Sachets</Text>
             <Text as="subtitle">
                Sachets are used for packaging ingredients for a meal kit.
             </Text>
          </SolidTile>
          <br />
-         <SolidTile
-            onClick={() => {
-               dispatch({
-                  type: 'SET_PACKAGING_ID',
-                  payload: '',
-               })
-               dispatch({
-                  type: 'SET_PACKAGING_TYPE',
-                  payload: 'ASSEMBLY_PACKAGE',
-               })
-
-               addTab('New Packaging', 'sachetPackaging')
-            }}
-         >
+         <SolidTile onClick={() => createPackagingHandler('ASSEMBLY_PACKAGE')}>
             <Text as="h1">Assembly Packet</Text>
             <Text as="subtitle">
                Assembly packet is used to assemble all the sacheted ingredients
