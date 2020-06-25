7053d43cfd15a1ec7396fed0f8ef57e741969db1
siddhantk232 <siddhantk232@gmail.com>
2020-06-25 13:49:24 +0530

    refactor: tunnels change: use TunnelHeader from UI lib

--- src/apps/inventory/views/Forms/PurchaseOrders/Tunnels/SelectSupplierItemTunnel/index.jsx
+++ src/apps/inventory/views/Forms/PurchaseOrders/Tunnels/SelectSupplierItemTunnel/index.jsx
@@ -4,26 +4,34 @@ import {
    ListOptions,
    ListSearch,
    useSingleList,
+   Loader,
+   TunnelHeader,
 } from '@dailykit/ui'
 import React, { useContext, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useSubscription } from '@apollo/react-hooks'
 
-import {
-   Spacer,
-   TunnelContainer,
-   TunnelHeader,
-} from '../../../../../components'
+import { TunnelContainer } from '../../../../../components'
 import { PurchaseOrderContext } from '../../../../../context/purchaseOrder'
+import { SUPPLIER_ITEMS_SUBSCRIPTION } from '../../../../../graphql'
 
 const address =
    'apps.inventory.views.forms.purchaseorders.tunnels.selectsupplieritemtunnel.'
 
-export default function AddressTunnel({ close, supplierItems }) {
+export default function AddressTunnel({ close }) {
    const { t } = useTranslation()
    const { purchaseOrderDispatch } = useContext(PurchaseOrderContext)
    const [search, setSearch] = useState('')
+   const [data, setData] = useState([])
 
-   const [list, current, selectOption] = useSingleList(supplierItems)
+   const [list, current, selectOption] = useSingleList(data)
+
+   const { loading } = useSubscription(SUPPLIER_ITEMS_SUBSCRIPTION, {
+      onSubscriptionData: input => {
+         const data = input.subscriptionData.data.supplierItems
+         setData(data)
+      },
+   })
 
    const handleNext = () => {
       const bulkItemAsShipped = current.bulkItems.find(
@@ -36,40 +44,45 @@ export default function AddressTunnel({ close, supplierItems }) {
       })
       close(1)
    }
+
+   if (loading) return <Loader />
+
    return (
-      <TunnelContainer>
+      <>
          <TunnelHeader
             title={t(address.concat('select supplier item'))}
-            next={handleNext}
             close={() => close(1)}
-            nextAction="Save"
+            right={{ title: 'Save', action: handleNext }}
          />
-
-         <Spacer />
-
-         <List>
-            {Object.keys(current).length > 0 ? (
-               <ListItem type="SSL1" title={current.name} />
-            ) : (
-               <ListSearch
-                  onChange={value => setSearch(value)}
-                  placeholder="type what you’re looking for..."
-               />
-            )}
-            <ListOptions>
-               {list
-                  .filter(option => option.name.toLowerCase().includes(search))
-                  .map(option => (
-                     <ListItem
-                        type="SSL1"
-                        key={option.id}
-                        title={option.name}
-                        isActive={option.id === current.id}
-                        onClick={() => selectOption('id', option.id)}
-                     />
-                  ))}
-            </ListOptions>
-         </List>
-      </TunnelContainer>
+         <TunnelContainer>
+            <List>
+               {Object.keys(current).length > 0 ? (
+                  <ListItem type="SSL1" title={current.name} />
+               ) : (
+                  <ListSearch
+                     onChange={value => setSearch(value)}
+                     placeholder="type what you’re looking for..."
+                  />
+               )}
+               <ListOptions>
+                  {list
+                     .filter(option =>
+                        option.name.toLowerCase().includes(search)
+                     )
+                     .map(option => (
+                        <ListItem
+                           type="SSL1"
+                           key={option.id}
+                           title={option.name}
+                           isActive={option.id === current.id}
+                           onClick={() => selectOption('id', option.id)}
+                        />
+                     ))}
+               </ListOptions>
+            </List>
+            <br />
+            <br />
+         </TunnelContainer>
+      </>
    )
 }
