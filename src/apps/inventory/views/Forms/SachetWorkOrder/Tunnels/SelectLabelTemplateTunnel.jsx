6797a9cb39782a4d2f028dda27bd57cb3f1ab542
siddhantk232 <siddhantk232@gmail.com>
2020-06-25 17:09:01 +0530

    fix: labelTemplate tunnel

--- src/apps/inventory/views/Forms/SachetWorkOrder/Tunnels/SelectLabelTemplateTunnel.jsx
+++ src/apps/inventory/views/Forms/SachetWorkOrder/Tunnels/SelectLabelTemplateTunnel.jsx
@@ -6,80 +6,90 @@ import {
    Tag,
    TagGroup,
    useMultiList,
+   TunnelHeader,
 } from '@dailykit/ui'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Spacer, TunnelContainer, TunnelHeader } from '../../../../components'
+import { TunnelContainer } from '../../../../components'
 import { SachetOrderContext } from '../../../../context/sachetOrder'
 
 const address = 'apps.inventory.views.forms.sachetworkorder.tunnels.'
 
 export default function SelectLabelTemplateTunnel({ close }) {
    const { t } = useTranslation()
-   const { sachetOrderState, sachetOrderDispatch } = useContext(
-      SachetOrderContext
-   )
+   const { sachetOrderDispatch } = useContext(SachetOrderContext)
 
    const [search, setSearch] = React.useState('')
+   const [data] = React.useState([
+      {
+         id: 1,
+         title: 'Slip Name',
+      },
+      { id: 2, title: 'Bar Code' },
+      {
+         id: 3,
+         title: 'Sachet Quantity',
+      },
+      { id: 4, title: 'Supplier Name' },
+      { id: 5, title: 'Packaging date' },
+   ])
 
-   // const [list, selected, selectOption] = useMultiList()
+   const [list, selected, selectOption] = useMultiList(data || [])
 
-   // useEffect(() => {
-   //    sachetOrderState.labelTemplates.forEach(temp => {
-   //       selectOption('id', temp.id)
-   //    })
-   // }, [])
+   const handleNext = () => {
+      sachetOrderDispatch({
+         type: 'SELECT_TEMPLATE_OPTIONS',
+         payload: selected,
+      })
+      close(1)
+   }
 
    return (
-      <TunnelContainer>
+      <>
          <TunnelHeader
             title={t(address.concat('select label templates'))}
-            // next={() => {
-            //    sachetOrderDispatch({
-            //       type: 'SELECT_TEMPLATE_OPTIONS',
-            //       payload: selected,
-            //    })
-            //    close(1)
-            // }}
             close={() => close(1)}
-            nextAction="Save"
+            right={{ title: 'Save', action: handleNext }}
          />
-
-         <Spacer />
-
-         {/* <List>
-            <ListSearch
-               onChange={value => setSearch(value)}
-               placeholder={t(address.concat("type what you're looking for"))}
-            />
-            {selected.length > 0 && (
-               <TagGroup style={{ margin: '8px 0' }}>
-                  {selected.map(option => (
-                     <Tag
-                        key={option.id}
-                        title={option.title}
-                        onClick={() => selectOption('id', option.id)}
-                     >
-                        {option.title}
-                     </Tag>
-                  ))}
-               </TagGroup>
-            )}
-            <ListOptions>
-               {list
-                  .filter(option => option.title.toLowerCase().includes(search))
-                  .map(option => (
-                     <ListItem
-                        type="MSL1"
-                        key={option.id}
-                        title={option.title}
-                        onClick={() => selectOption('id', option.id)}
-                        isActive={selected.find(item => item.id === option.id)}
-                     />
-                  ))}
-            </ListOptions>
-         </List> */}
-      </TunnelContainer>
+         <TunnelContainer>
+            <List>
+               <ListSearch
+                  onChange={value => setSearch(value)}
+                  placeholder="type what you’re looking for..."
+               />
+               {selected.length > 0 && (
+                  <TagGroup style={{ margin: '8px 0' }}>
+                     {selected.map(option => (
+                        <Tag
+                           key={option.id}
+                           title={option.title}
+                           onClick={() => selectOption('id', option.id)}
+                        >
+                           {option.title}
+                        </Tag>
+                     ))}
+                  </TagGroup>
+               )}
+               <ListOptions>
+                  {list
+                     .filter(option =>
+                        option.title.toLowerCase().includes(search)
+                     )
+                     .map(option => (
+                        <ListItem
+                           type="MSL1"
+                           key={option.id}
+                           title={option.title}
+                           onClick={() => selectOption('id', option.id)}
+                           isActive={selected.find(
+                              item => item.id === option.id
+                           )}
+                        />
+                     ))}
+               </ListOptions>
+            </List>
+         </TunnelContainer>
+      </>
    )
 }
