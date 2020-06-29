5070b13bb3aee96797ff482b29a5559f6a12afbd
Praveen Bisht <prvnbist@gmail.com>
2020-06-29 19:58:25 +0530

    refactor: removed company id

--- src/apps/order/components/DeliveryConfig/index.jsx
+++ src/apps/order/components/DeliveryConfig/index.jsx
@@ -48,7 +48,6 @@ export const DeliveryConfig = () => {
             orderId: delivery_config.orderId,
             selectedDeliveryService: {
                logo: service.logo,
-               id: service.details.id,
                name: service.companyName,
             },
          },
