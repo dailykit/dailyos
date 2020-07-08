8b205b3d4ed171bf8dc624cb558365c88c92a2ab
siddhantk232 <siddhantk232@gmail.com>
2020-07-08 15:21:58 +0530

    feat: packagings query

--- src/apps/inventory/packagingHub/graphql/queries.js
+++ src/apps/inventory/packagingHub/graphql/queries.js
@@ -9,3 +9,23 @@ export const CATEGORIES = gql`
       }
    }
 `
+
+export const PACKAGINGS = gql`
+   query Packaging($id: Int!) {
+      packagingHub_packaging(where: { packagingTypeId: { _eq: $id } }) {
+         id
+         packagingName
+         assets
+         height
+         length
+         width
+         packagingCompanyBrand {
+            name
+         }
+         thickness
+         packagingPurchaseOptions(order_by: { quantity: asc }) {
+            salesPrice
+         }
+      }
+   }
+`
--- src/apps/inventory/packagingHub/views/Products/index.jsx
+++ src/apps/inventory/packagingHub/views/Products/index.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Loader } from '@dailykit/ui'
+import { useQuery } from '@apollo/react-hooks'
+
+import { Context } from '../../../context/tabs'
+import { PACKAGINGS } from '../../graphql'
 
 export default function PackagingHubProducts() {
+   const {
+      state: {
+         current: { id },
+      },
+   } = useContext(Context)
+
+   const { data, loading, error } = useQuery(PACKAGINGS, { variables: id })
+
+   if (error) return <p>{error.message}</p>
+   if (loading) return <Loader />
+
+   const { packagingHub_packaging: packagings = [] } = data
+   console.log(packagings)
+
    return (
       <div>
          <h1>Products View</h1>
