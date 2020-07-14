6002599f1106dd41a70878db074b680498ac3785
siddhantk232 <siddhantk232@gmail.com>
2020-07-14 16:00:25 +0530

    feat: basic Table UI

--- src/apps/inventory/packagingHub/components/Cart/CartTunnel.jsx
+++ src/apps/inventory/packagingHub/components/Cart/CartTunnel.jsx
@@ -1,16 +1,91 @@
 import React from 'react'
-import { TunnelHeader } from '@dailykit/ui'
+import {
+   TunnelHeader,
+   Loader,
+   Table,
+   TableHead,
+   TableBody,
+   TableRow,
+   TableCell,
+} from '@dailykit/ui'
+import { useQuery } from '@apollo/react-hooks'
+import { toast } from 'react-toastify'
+import styled from 'styled-components'
 
 import { TunnelContainer } from '../../../components'
 
+import { CART_ITEMS } from '../../graphql'
+
 export default function AddressTunnel({ close }) {
+   const {
+      loading,
+      data: { organizationPurchaseOrders_purchaseOrderItem: items = [] } = {},
+   } = useQuery(CART_ITEMS, {
+      onError: error => {
+         toast.error(error.message)
+         console.log(error)
+      },
+   })
+
    return (
       <>
          <TunnelHeader title="Purchase Orders" close={() => close(1)} />
 
          <TunnelContainer>
-            <h1>I am working...</h1>
+            {loading ? <Loader /> : <Content items={items} />}
          </TunnelContainer>
       </>
    )
 }
+
+function Content({ items }) {
+   return (
+      <Wrapper>
+         <h2>{items.length} items</h2>
+         <br />
+
+         {items.length ? (
+            <Table>
+               <TableHead>
+                  <TableRow>
+                     <TableCell>Packaging Item</TableCell>
+                     <TableCell>Package</TableCell>
+                     <TableCell>Quantity</TableCell>
+                     <TableCell>Total Price</TableCell>
+                     <TableCell />
+                  </TableRow>
+               </TableHead>
+               <TableBody>
+                  {items.map(item => (
+                     <TableRow key={item.id}>
+                        <TableCell>{item.packaging.packagingName}</TableCell>
+                        <TableCell>{item.purchaseOption.quantity}</TableCell>
+                        <TableCell>{item.multiplier}</TableCell>
+                        <TableCell>
+                           ${' '}
+                           {`${
+                              item.purchaseOption.salesPrice * item.multiplier
+                           }`.slice(0.5)}
+                        </TableCell>
+                        1
+                        <TableCell />
+                     </TableRow>
+                  ))}
+               </TableBody>
+            </Table>
+         ) : (
+            <h1>Your Cart is empty. Go buy something !</h1>
+         )}
+      </Wrapper>
+   )
+}
+
+const Wrapper = styled.div`
+   width: 100%;
+
+   h1,
+   h2 {
+      font-size: 16px;
+      color: #555b6e;
+   }
+`
