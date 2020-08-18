202dc327f1742cfa93e5fc0978db7f8d6df2a5ed
Gokul Reddy <gokulreddy98@gmail.com>
2020-08-18 17:09:20 +0530

    feat:notifications sidebar

--- src/apps/settings/views/Listings/Notifications/index.jsx
+++ src/apps/settings/views/Listings/Notifications/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-
+import { NOTIFICATION_TYPES } from '../../../graphql/subscriptions/index'
+import { useSubscription } from '@apollo/react-hooks'
 import { useTabs } from '../../../context'
+import { Loader, Table, TableHead, TableCell, TableRow } from '@dailykit/ui'
+import { StyledLayout, Container, Grid, Link } from './styled'
+
+const Layout = props => {
+   let tables = () => {
+      props.list.map(item => {
+         return <h1>{item.app}</h1>
+      })
+   }
+   return (
+      <>
+         <h1>{props.title + ' App'}</h1>
+         {tables}
+      </>
+   )
+}
 
 const Notifications = () => {
    const history = useHistory()
    const { tabs, addTab } = useTabs()
+
    React.useEffect(() => {
       const tabIndex =
          tabs.findIndex(item => item.path === `/settings/notifications`) || {}
@@ -14,7 +32,45 @@ const Notifications = () => {
       }
    }, [history, tabs])
 
-   return <>text</>
-}
+   const { loading, error, data } = useSubscription(NOTIFICATION_TYPES)
+   if (loading) {
+      return <Loader />
+   }
+   if (error) {
+      return console.log(error)
+   }
 
+   if (data) {
+      const notifications = data.notificationTypes
+
+      let apps = new Set()
+      notifications.map(notification => {
+         apps.add(notification.app)
+      })
+      apps = [...apps]
+      let sideItems = apps.map(app => {
+         return (
+            <Grid cols="1" key={app}>
+               <Link href="#order">{app}</Link>
+            </Grid>
+         )
+      })
+
+      return (
+         <>
+            <StyledLayout>
+               <Container height="100" color="#F3F3F3">
+                  <Container top="30">{sideItems}</Container>
+               </Container>
+
+               <div>
+                  <div id="Order"></div>
+                  <div id="Recipe"></div>
+                  <div id="Settings"></div>
+               </div>
+            </StyledLayout>
+         </>
+      )
+   }
+}
 export default Notifications
--- src/apps/settings/views/Listings/Notifications/styled.js
+++ src/apps/settings/views/Listings/Notifications/styled.js
@@ -2,17 +2,17 @@ import styled from 'styled-components'
 
 export const StyledLayout = styled.div`
    display: grid;
-   grid-template-columns: 300px 1fr;
+   grid-template-columns: 20vw 1fr;
 `
 
 export const Grid = styled.div`
    display: grid;
    grid-template-columns: repeat(${props => props.cols || 2}, 1fr);
-   grid-gap: ${props => props.gap || 8}px;
+   grid-gap: ${props => props.gap}em;
 `
 
 export const Container = styled.div`
-   margin-top: ${props => props.top || 0}px;
+   margin-top: ${props => props.top || 0}vh;
    margin-bottom: ${props => props.bottom || 0}px;
    margin-left: ${props => props.left || 0}px;
    margin-right: ${props => props.right || 0}px;
@@ -23,6 +23,7 @@ export const Container = styled.div`
    width: ${props => (props.width ? props.width + 'px' : 'auto')};
    height: ${props => (props.height ? props.height + 'vh' : 'auto')};
    max-width: ${props => (props.maxWidth ? props.maxWidth + 'px' : '100%')};
+   background-color: ${props => (props.color ? props.color : 'white')};
    flex: 4;
 `
 
@@ -38,4 +39,10 @@ export const TunnelBody = styled.div`
    padding: 32px;
    height: calc(100% - 106px);
    overflow: auto;
+`
+
+export const Link = styled.a`
+   color: black;
+   text-decoration: none;
+   padding: 30px;
 `
