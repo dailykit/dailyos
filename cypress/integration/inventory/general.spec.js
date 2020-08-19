f1ec075d4a98b5350b94137ed69c8014bc27b242
siddhantk232 <siddhantk232@gmail.com>
2020-08-19 12:10:50 +0530

    feat: old specs

--- cypress/integration/inventory/general.spec.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable spaced-comment */
-/// <reference types="cypress" />
-
-context('General tests for inventory app', () => {
-   it('should login and visit inventory app', () => {
-      cy.gotoApp('inventory').get('h1').should('be.visible')
-   })
-})
