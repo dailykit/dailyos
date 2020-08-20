9545ba60b70e116fec420f506c66cd651a9aa682
siddhantk232 <siddhantk232@gmail.com>
2020-08-20 17:22:25 +0530

    feat: updating DB directly now

--- src/apps/inventory/views/Forms/BulkWorkOrder/Tunnels/SelectInputBulkItemTunnel.jsx
+++ src/apps/inventory/views/Forms/BulkWorkOrder/Tunnels/SelectInputBulkItemTunnel.jsx
@@ -4,31 +4,75 @@ import {
    ListOptions,
    ListSearch,
    useSingleList,
+   Loader,
+   TunnelHeader,
 } from '@dailykit/ui'
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useSubscription, useMutation } from '@apollo/react-hooks'
+import { toast } from 'react-toastify'
 
-import { Spacer, TunnelContainer, TunnelHeader } from '../../../../components'
-import { BulkOrderContext } from '../../../../context/bulkOrder'
+import { Spacer, TunnelContainer } from '../../../../components'
+import {
+   GET_BULK_ITEMS_SUBSCRIPTION,
+   UPDATE_BULK_WORK_ORDER,
+} from '../../../../graphql'
 
 const address = 'apps.inventory.views.forms.bulkworkorder.tunnels.'
 
-export default function SelectInputBulkItemTunnel({ close, bulkItems }) {
+export default function SelectInputBulkItemTunnel({ close, state }) {
    const { t } = useTranslation()
-   const { bulkOrderDispatch } = useContext(BulkOrderContext)
    const [search, setSearch] = useState('')
-
+   const [bulkItems, setBulkItems] = useState([])
    const [list, current, selectOption] = useSingleList(bulkItems)
+
+   const { loading } = useSubscription(GET_BULK_ITEMS_SUBSCRIPTION, {
+      variables: {
+         supplierItemId: state.supplierItem.id,
+      },
+      onError: error => {
+         console.log(error)
+         toast.error(error.message)
+      },
+      onSubscriptionData: data => {
+         const { bulkItems } = data.subscriptionData.data
+
+         setBulkItems(bulkItems)
+      },
+   })
+
+   const [updateBulkWorkOrder] = useMutation(UPDATE_BULK_WORK_ORDER, {
+      onError: error => {
+         console.log(error)
+         toast.error(error.message)
+      },
+      onCompleted: () => {
+         toast.success('Input Bulk Item added!')
+         close(1)
+      },
+   })
+
+   const handleSave = () => {
+      if (!current || !current.id) return toast.error('Please select an item.')
+
+      updateBulkWorkOrder({
+         variables: {
+            id: state.id,
+            object: {
+               inputBulkItemId: current.id,
+            },
+         },
+      })
+   }
+
+   if (loading) return <Loader />
+
    return (
       <TunnelContainer>
          <TunnelHeader
             title={t(address.concat('select input bulk item processing'))}
-            next={() => {
-               bulkOrderDispatch({ type: 'ADD_INPUT_ITEM', payload: current })
-               close(1)
-            }}
             close={() => close(1)}
-            nextAction="Save"
+            right={{ action: handleSave, title: 'Save' }}
          />
 
          <Spacer />
