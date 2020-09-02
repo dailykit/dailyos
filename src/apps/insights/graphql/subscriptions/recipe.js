f661ebca83fadf68d3d3730a2ac74f103054c7d7
siddhant <siddhantk232@gmail.com>
2020-09-02 20:40:47 +0530

    feat: new queries
    for testing only

--- src/apps/insights/graphql/subscriptions/recipe.js
+++ src/apps/insights/graphql/subscriptions/recipe.js
@@ -1,8 +1,15 @@
 import gql from 'graphql-tag'
 
 export const RECIPE_SALE_COUNT = gql`
-   subscription RecipeSaleCount {
-      TotalRecipeSales: simple_recipe_sale_meal_kit {
+   query RecipeSaleCount {
+      RecipeSalesInMealKits: insights_simple_recipe_sale_meal_kit {
+         data: count
+         label: recipe {
+            id
+            name
+         }
+      }
+      RecipeSalesInReadyToEats: insights_simple_recipe_sale_ready_to_eat {
          data: count
          label: recipe {
             id
@@ -10,4 +17,31 @@ export const RECIPE_SALE_COUNT = gql`
          }
       }
    }
+`
+
+const query = `
+   query mealKitProductsOrders(
+      $includeSimpleRecipeProductName: Boolean!
+      $includeDefaultCartItem: Boolean!
+      $options: order_orderMealKitProduct_bool_exp
+   ) {
+      orderMealKitProductsAggregate(where: $options) {
+         aggregate {
+            count
+         }
+         nodes {
+            id
+            simpleRecipeProduct {
+               id
+               name @include(if: $includeSimpleRecipeProductName)
+               defaultCartItem
+                  @include(if: $includeDefaultCartItem)
+            }
+         }
+      }
+   }
+`
+
+export const SAMPLE_QUERY = gql`
+   ${query}
 `
