85602492780b498bbbcd3598d8d4ef6b5c7bf18d
siddhant <siddhantk232@gmail.com>
2020-09-02 20:45:01 +0530

    feat: view cleaned

--- src/apps/insights/views/RecipeInsight/index.jsx
+++ src/apps/insights/views/RecipeInsight/index.jsx
@@ -1,52 +1,13 @@
 import React from 'react'
-import { Pie, Bar } from 'react-chartjs-2'
-import { useSubscription } from '@apollo/react-hooks'
-import { Loader } from '@dailykit/ui'
-import G2C from 'graphql2chartjs'
 
 import { StyledHeader, StyledWrapper } from '../styled'
-import { RECIPE_SALE_COUNT } from '../../graphql'
 
 const ReferralPlansListing = () => {
-   const { data, loading } = useSubscription(RECIPE_SALE_COUNT, {
-      onError: error => {
-         console.log(error)
-      },
-   })
-
-   if (loading) return <Loader />
-
-   const g2c = new G2C(data, (_, data) => {
-      return {
-         chartType: 'bar',
-         label: data.label.name,
-      }
-   })
-
    return (
       <StyledWrapper>
          <StyledHeader>
             <h1>Reicpe Insights</h1>
          </StyledHeader>
-
-         <Bar
-            data={g2c.data}
-            options={{
-               title: {
-                  display: true,
-                  fontSize: 24,
-                  text: 'Recipe sale in MealKit products',
-               },
-               legend: { display: false },
-               maintainAspectRatio: false,
-               elements: {
-                  rectangle: {
-                     backgroundColor: ['#E9DAFF', '#D9EBFB'],
-                     borderWidth: 2,
-                  },
-               },
-            }}
-         />
       </StyledWrapper>
    )
 }
