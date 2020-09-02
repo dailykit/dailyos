358e90c188077df39569f693c9689f8975f480c1
siddhant <siddhantk232@gmail.com>
2020-09-03 01:42:51 +0530

    feat: useInsight test

--- src/apps/insights/views/RecipeInsight/index.jsx
+++ src/apps/insights/views/RecipeInsight/index.jsx
@@ -1,39 +1,18 @@
 import React from 'react'
-import { ReactTabulator } from 'react-tabulator'
-import { useQuery } from '@apollo/react-hooks'
-import { Loader } from '@dailykit/ui'
-
-import 'react-tabulator/css/bootstrap/tabulator_bootstrap.min.css'
-import 'react-tabulator/lib/styles.css'
-import '../../../../shared/styled/tableStyles.css'
 
 import { StyledHeader, StyledWrapper } from '../styled'
-import { SAMPLE_QUERY } from '../../graphql/subscriptions/recipe'
-import { transformer } from '../../utils/transformer'
+import { useInsights } from '../../hooks/useInsights'
 
 const ReferralPlansListing = () => {
-   const { loading, data } = useQuery(SAMPLE_QUERY, {
-      variables: {
-         includeSimpleRecipeProductName: true,
-         includeDefaultCartItem: true,
-         options: null,
-      },
-   })
+   const data = useInsights('e9b59cd3-7a22-426b-bd63-37ff70e76a45')
 
-   if (loading) return <Loader />
+   console.log(data)
 
-   const tableData = transformer(data, 'orderMealKitProductsAggregate')
-   console.log(tableData)
    return (
       <StyledWrapper>
          <StyledHeader>
             <h1>Reicpe Insights</h1>
          </StyledHeader>
-         <ReactTabulator
-            data={tableData || []}
-            options={{ autoColumns: true, layout: 'fitColumns' }}
-            columns={[]}
-         />
       </StyledWrapper>
    )
 }
