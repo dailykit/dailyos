cdf6e699c6ccd10009f198ad2fee46b4443a42d8
siddhant <siddhantk232@gmail.com>
2020-09-03 21:53:56 +0530

    feat: chart+tale example using useInsights hook

--- src/apps/insights/views/RecipeInsight/index.jsx
+++ src/apps/insights/views/RecipeInsight/index.jsx
@@ -1,18 +1,68 @@
 import React from 'react'
+import {
+   HorizontalBar,
+   Bar,
+   Pie,
+   Doughnut,
+   Line,
+   Radar,
+   Polar,
+} from 'react-chartjs-2'
+import { ReactTabulator } from 'react-tabulator'
 
+import 'react-tabulator/css/bootstrap/tabulator_bootstrap.min.css'
+import 'react-tabulator/lib/styles.css'
+import '../../../../shared/styled/tableStyles.css'
+
+import { colors } from '../../utils/colors'
 import { StyledHeader, StyledWrapper } from '../styled'
 import { useInsights } from '../../hooks/useInsights'
 
 const ReferralPlansListing = () => {
-   const data = useInsights('e9b59cd3-7a22-426b-bd63-37ff70e76a45')
-
-   console.log(data)
+   const { chartData, tableData } = useInsights(
+      'e9b59cd3-7a22-426b-bd63-37ff70e76a45',
+      {
+         chart: {
+            type: 'labeled',
+            x: 'name',
+            name: 'Total Inventory Product Sale',
+         },
+      }
+   )
 
    return (
       <StyledWrapper>
          <StyledHeader>
             <h1>Reicpe Insights</h1>
          </StyledHeader>
+
+         <Radar
+            data={chartData}
+            options={{
+               elements: {
+                  rectangle: { backgroundColor: colors },
+                  arc: { backgroundColor: colors },
+               },
+               legend: { display: false },
+               title: {
+                  display: true,
+                  text: 'Total inventory product sales',
+                  fontSize: 25,
+               },
+            }}
+         />
+
+         <br />
+         <br />
+         <hr />
+         <br />
+         <br />
+
+         <ReactTabulator
+            columns={[]}
+            options={{ autoColumns: true }}
+            data={tableData}
+         />
       </StyledWrapper>
    )
 }
