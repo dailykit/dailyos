f1330e9251c38229bdc33a35491b6f3cf0470701
siddhant <siddhantk232@gmail.com>
2020-09-10 18:02:34 +0530

    feat: Dropdown components

---
+++ src/apps/insights/components/DropdownMenu/index.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+import { ChevronDown } from '../../../../shared/assets/icons'
+
+export const Dropdown = ({ title, children, view, setView }) => {
+   const [show, setShow] = useState(false)
+   return (
+      <div style={{ position: 'relative' }}>
+         <DropdownButton onClick={() => setShow(value => !value)}>
+            <p>{title}</p>
+
+            <ChevronDown />
+         </DropdownButton>
+
+         {show && <Box>{children}</Box>}
+      </div>
+   )
+}
+
+export const DropdownItem = ({ leftIcon, rightIcon, onClick, children }) => {
+   return (
+      <StyledItem onClick={onClick}>
+         {leftIcon}
+         <div style={{ margin: '0 8px' }} onClick={() => {}}>
+            {children}
+         </div>
+         {rightIcon}
+      </StyledItem>
+   )
+}
+
+const DropdownButton = styled.button`
+   padding: 8px 16px;
+   color: #00a7e1;
+   background-color: #fff;
+   border: 1px solid #f3f3f3;
+   border-radius: 2px;
+   width: 10vw;
+
+   font-size: 18px;
+   font-weight: 500;
+
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   cursor: pointer;
+
+   margin-bottom: 12px;
+`
+const Box = styled.div`
+   background-color: #fff;
+   padding: 8px;
+   border-radius: 2px;
+
+   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+   border: 1px solid #f3f3f3;
+
+   position: absolute;
+   top: 90%;
+
+   z-index: 12;
+`
+
+const StyledItem = styled.div`
+   display: flex;
+   align-items: center;
+   border-radius: 2px;
+
+   color: #888d9d;
+   padding: 12px;
+   min-width: 10vw;
+   cursor: pointer;
+
+   &:hover {
+      background-color: #f3f3f3;
+   }
+`
