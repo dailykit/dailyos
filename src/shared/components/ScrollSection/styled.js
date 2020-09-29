f6643babfb345603fc9c1e71ae8eff11278c18de
Praveen Bisht <prvnbist@gmail.com>
2020-09-29 18:39:25 +0530

    feat(component): ScrollSection

---
+++ src/shared/components/ScrollSection/styled.js
@@ -0,0 +1,58 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div(
+   ({ height }) => css`
+      overflow: hidden;
+      display: grid;
+      grid-template-columns: 240px 1fr;
+      aside,
+      > main {
+         overflow-y: auto;
+         height: ${height || '100vh'};
+      }
+   `
+)
+
+export const Sidebar = styled.aside`
+   padding: 6px;
+   border-radius: 2px;
+   background: #d9e9f1;
+   > ul > li {
+      list-style: none;
+      margin-bottom: 4px;
+   }
+   > ul > li > ul {
+      margin-left: 16px;
+      padding-top: 4px;
+      li {
+         list-style: none;
+         + li {
+            margin-top: 4px;
+         }
+      }
+   }
+   a {
+      height: 32px;
+      display: flex;
+      color: #1f495e;
+      padding: 0 8px;
+      align-items: center;
+      border-radius: 2px;
+      text-decoration: none;
+      transition: 0.1s linear;
+      :hover,
+      &.active {
+         color: #fff;
+         background: #1bb9f0;
+      }
+   }
+`
+
+export const Content = styled.main`
+   width: 100%;
+   padding: 16px;
+   scroll-behavior: smooth;
+   > div {
+      max-width: 560px;
+   }
+`
