77f0ff6a33333802534375e38b7b0f8b40c07de4
Praveen Bisht <prvnbist@gmail.com>
2020-10-01 16:01:38 +0530

    feat(brand): create brand

--- src/apps/brands/views/Listings/brands/index.jsx
+++ src/apps/brands/views/Listings/brands/index.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
-import { IconButton, Text } from '@dailykit/ui'
-import { useSubscription } from '@apollo/react-hooks'
+import { toast } from 'react-toastify'
+import { useMutation, useSubscription } from '@apollo/react-hooks'
+import {
+   Text,
+   Input,
+   Spacer,
+   PlusIcon,
+   IconButton,
+   Tunnel,
+   Tunnels,
+   useTunnel,
+   TunnelHeader,
+} from '@dailykit/ui'
 import { reactFormatter, ReactTabulator } from '@dailykit/react-tabulator'
 
 import { BRANDS } from '../../../graphql'
 import { useTabs } from '../../../context'
 import tableOptions from '../../../tableOption'
 import { StyledWrapper, StyledHeader } from '../styled'
-import { InlineLoader } from '../../../../../shared/components'
 import { EditIcon } from '../../../../../shared/assets/icons'
+import { InlineLoader, Flex } from '../../../../../shared/components'
 
 export const Brands = () => {
    const tableRef = React.useRef()
    const { tab, addTab } = useTabs()
+   const [form, setForm] = React.useState({
+      title: '',
+      domain: '',
+   })
+   const [create] = useMutation(BRANDS.CREATE_BRAND, {
+      onCompleted: () => {
+         setForm({
+            title: '',
+            domain: '',
+         })
+         closeTunnel(1)
+         toast.success('Successfully created the brand!')
+      },
+      onError: () =>
+         toast.success('Failed to create the brand, please try again!'),
+   })
+   const [tunnels, openTunnel, closeTunnel] = useTunnel(1)
    const { error, loading, data: { brands = {} } = {} } = useSubscription(
       BRANDS.LIST
    )
@@ -62,11 +90,32 @@ export const Brands = () => {
       []
    )
 
+   const save = () => {
+      if (form.title && form.domain) {
+         return create({
+            variables: {
+               object: {
+                  ...form,
+               },
+            },
+         })
+      }
+      return toast.error('Title and Domain is required!')
+   }
+
+   const handleChange = e => {
+      const { name, value } = e.target
+      setForm(form => ({ ...form, [name]: value }))
+   }
+
    if (error) return <div>Something went wrong, please refresh!</div>
    return (
       <StyledWrapper>
          <StyledHeader>
             <Text as="h2">Brands ({brands?.aggregate?.count || 0})</Text>
+            <IconButton type="solid" onClick={() => openTunnel(1)}>
+               <PlusIcon />
+            </IconButton>
          </StyledHeader>
          {loading ? (
             <InlineLoader />
@@ -84,6 +133,32 @@ export const Brands = () => {
                )}
             </>
          )}
+         <Tunnels tunnels={tunnels}>
+            <Tunnel layer={1} size="sm">
+               <TunnelHeader
+                  title="Add Brand"
+                  right={{ action: save, title: 'Save' }}
+                  close={() => closeTunnel(1)}
+               />
+               <Flex padding="16px">
+                  <Input
+                     type="text"
+                     label="Title"
+                     name="title"
+                     value={form.title}
+                     onChange={e => handleChange(e)}
+                  />
+                  <Spacer size="24px" />
+                  <Input
+                     type="text"
+                     label="Domain"
+                     name="domain"
+                     value={form.domain}
+                     onChange={e => handleChange(e)}
+                  />
+               </Flex>
+            </Tunnel>
+         </Tunnels>
       </StyledWrapper>
    )
 }
