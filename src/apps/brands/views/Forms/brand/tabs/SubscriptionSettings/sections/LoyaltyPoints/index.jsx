109c40028a645466360e1a46d5d7ceaa4cb0ff86
Praveen Bisht <prvnbist@gmail.com>
2020-10-06 18:59:28 +0530

    feat(brand): more subscription settings

---
+++ src/apps/brands/views/Forms/brand/tabs/SubscriptionSettings/sections/LoyaltyPoints/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { isEmpty } from 'lodash'
+import { useParams } from 'react-router-dom'
+import { useSubscription } from '@apollo/react-hooks'
+import { TextButton, Text, Spacer, Toggle } from '@dailykit/ui'
+
+import { BRANDS } from '../../../../../../../graphql'
+import { Flex } from '../../../../../../../../../shared/components'
+
+export const LoyaltyPoints = ({ update }) => {
+   const params = useParams()
+   const [settingId, setSettingId] = React.useState(null)
+   const [isAvailable, setIsAvailable] = React.useState(false)
+   useSubscription(BRANDS.SUBSCRIPTION_SETTING, {
+      variables: {
+         identifier: { _eq: 'Loyalty Points' },
+         type: { _eq: 'rewards' },
+      },
+      onSubscriptionData: ({
+         subscriptionData: { data: { subscriptionSetting = [] } = {} } = {},
+      }) => {
+         if (!isEmpty(subscriptionSetting)) {
+            const index = subscriptionSetting.findIndex(
+               node => node?.brand?.brandId === Number(params.id)
+            )
+
+            if (index === -1) {
+               const { id } = subscriptionSetting[0]
+               setSettingId(id)
+               return
+            }
+            const { brand, id } = subscriptionSetting[index]
+            setSettingId(id)
+            if ('isAvailable' in brand.value) {
+               setIsAvailable(brand.value.isAvailable)
+            }
+         }
+      },
+   })
+
+   const updateSetting = React.useCallback(() => {
+      update({ id: settingId, value: { isAvailable } })
+   }, [isAvailable, settingId])
+
+   return (
+      <div id="Loyalty Points">
+         <Text as="h3">Loyalty Points Availablility</Text>
+         <Spacer size="8px" />
+         <Flex container alignItems="center" justifyContent="space-between">
+            <Toggle
+               label="Available"
+               checked={isAvailable}
+               setChecked={setIsAvailable}
+            />
+            <TextButton size="sm" type="outline" onClick={updateSetting}>
+               Update
+            </TextButton>
+         </Flex>
+      </div>
+   )
+}
