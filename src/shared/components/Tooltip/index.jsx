20d87ee5c2e35cae21c39d94829932d56a3c9ecb
Praveen Bisht <prvnbist@gmail.com>
2020-10-12 15:53:18 +0530

    feat: Tooltip component

---
+++ src/shared/components/Tooltip/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Spacer } from '@dailykit/ui'
+import styled from 'styled-components'
+import ReactTooltip from 'react-tooltip'
+
+import { useTooltip } from '../../providers'
+import { TooltipIcon } from '../../assets/icons'
+
+export const Tooltip = ({
+   type = null,
+   effect = null,
+   position = null,
+   identifier = null,
+}) => {
+   const { tooltip } = useTooltip()
+
+   if (!identifier) {
+      console.error('Identifier is required!')
+   }
+   return (
+      <Styles.Tooltip>
+         <a data-tip data-for={identifier}>
+            <TooltipIcon />
+         </a>
+         <ReactTooltip
+            id={identifier}
+            clickable={true}
+            type={type || 'info'}
+            place={position || 'top'}
+            effect={effect || 'solid'}
+            overridePosition={({ left, top }, _e, _t, node) => {
+               return {
+                  top,
+                  left: typeof node === 'string' ? left : Math.max(left, 0),
+               }
+            }}
+         >
+            <p>{tooltip(identifier)?.description}</p>
+            <Spacer size="12px" />
+            {tooltip(identifier)?.link && (
+               <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={tooltip(identifier)?.link}
+               >
+                  View Docs
+               </a>
+            )}
+         </ReactTooltip>
+      </Styles.Tooltip>
+   )
+}
+
+const Styles = {
+   Tooltip: styled.span`
+      height: 24px;
+      margin-left: 4px;
+      > a {
+         height: 24px;
+         display: block;
+         cursor: pointer;
+      }
+      > div {
+         max-width: 250px;
+         &.__react_component_tooltip {
+            padding: 8px 16px;
+         }
+         a {
+            color: white;
+            float: right;
+         }
+      }
+   `,
+}
