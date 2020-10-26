401bd7c2b76310961aba5f286b806847676e9470
siddhant <siddhantk232@gmail.com>
2020-10-26 13:54:13 +0530

    feat: use `<Ranger />`

--- src/apps/inventory/views/Forms/Item/RealtimeView.jsx
+++ src/apps/inventory/views/Forms/Item/RealtimeView.jsx
@@ -1,6 +1,7 @@
 import { Flex } from '@dailykit/ui'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Ranger } from '../../../../../shared/components/Ranger'
 import { DataCard } from '../../../components'
 
 const address = 'apps.inventory.views.forms.item.'
@@ -11,23 +12,31 @@ export default function RealTimeView({ proc }) {
    if (!proc) return null
 
    return (
-      <Flex container style={{ flexWrap: 'wrap' }}>
-         <DataCard
-            title={t(address.concat('awaiting'))}
-            quantity={`${proc.awaiting} ${proc.unit}`}
+      <Flex margin="48px 48px 0 0">
+         <Ranger
+            label="On hand qty"
+            max={proc.maxLevel}
+            min={proc.parLevel}
+            minLabel="Par Level"
+            maxLabel="Max Inventory qty"
+            value={proc.onHand}
+            unit={proc.unit}
          />
-         <DataCard
-            title={t(address.concat('commited'))}
-            quantity={`${proc.committed} ${proc.unit}`}
-         />
-         <DataCard
-            title={t(address.concat('consumed'))}
-            quantity={`${proc.consumed} ${proc.unit}`}
-         />
-         <DataCard
-            title={t(address.concat('on hand'))}
-            quantity={`${proc.onHand} ${proc.unit}`}
-         />
+
+         <Flex container style={{ flexWrap: 'wrap' }}>
+            <DataCard
+               title={t(address.concat('awaiting'))}
+               quantity={`${proc.awaiting} ${proc.unit}`}
+            />
+            <DataCard
+               title={t(address.concat('commited'))}
+               quantity={`${proc.committed} ${proc.unit}`}
+            />
+            <DataCard
+               title={t(address.concat('consumed'))}
+               quantity={`${proc.consumed} ${proc.unit}`}
+            />
+         </Flex>
       </Flex>
    )
 }
