ef36c95c38eaccc6db1a3f1f36492411600e08bb
siddhant <siddhantk232@gmail.com>
2020-11-02 16:50:39 +0530

    feat: using radio buttons in purchase order listing

--- src/apps/inventory/views/Listings/PurchaseOrders/index.jsx
+++ src/apps/inventory/views/Listings/PurchaseOrders/index.jsx
@@ -1,9 +1,7 @@
-import { useSubscription } from '@apollo/react-hooks'
-import { reactFormatter, ReactTabulator } from '@dailykit/react-tabulator'
 import {
    ComboButton,
    Flex,
-   Loader,
+   RadioGroup,
    Spacer,
    Tag,
    Text,
@@ -12,91 +10,23 @@ import {
    Tunnels,
    useTunnel,
 } from '@dailykit/ui'
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { toast } from 'react-toastify'
-import { v4 as uuid } from 'uuid'
 import { Tooltip } from '../../../../../shared/components/Tooltip'
-import { useTooltip } from '../../../../../shared/providers'
-import { logger } from '../../../../../shared/utils'
 import { AddIcon } from '../../../assets/icons'
-import { GENERAL_ERROR_MESSAGE } from '../../../constants/errorMessages'
-import { useTabs } from '../../../context'
-import { PURCHASE_ORDERS_SUBSCRIPTION } from '../../../graphql'
-import tableOptions from '../tableOption'
+import PackagingPurchaseOrders from './packaging'
 import SelectPurchaseOrderTypeTunnel from './SelectPurchaseOrderTypeTunnel'
+import ItemPurchaseOrders from './supplierItem'
 
 const address = 'apps.inventory.views.listings.purchaseorders.'
 
 export default function PurchaseOrders() {
    const { t } = useTranslation()
-   const { addTab } = useTabs()
-   const { tooltip } = useTooltip()
+   const [view, setView] = useState('Supplier Items')
 
    const [tunnels, openTunnel, closeTunnel] = useTunnel(1)
 
-   const {
-      loading,
-      data: { purchaseOrderItems = [] } = {},
-      error,
-   } = useSubscription(PURCHASE_ORDERS_SUBSCRIPTION)
-
-   if (error) {
-      toast.error(GENERAL_ERROR_MESSAGE)
-      logger(error)
-   }
-
    const tableRef = React.useRef()
-
-   const openForm = (_, cell) => {
-      const { id, type } = cell.getData()
-      const tabTitle = `Purchase Order-${uuid().substring(30)}`
-      if (type === 'PACKAGING') {
-         addTab(tabTitle, `/inventory/purchase-orders/packaging/${id}`)
-      }
-
-      if (type === 'SUPPLIER_ITEM') {
-         addTab(tabTitle, `/inventory/purchase-orders/item/${id}`)
-      }
-   }
-
-   const columns = [
-      {
-         title: 'Status',
-         field: 'status',
-         headerFilter: true,
-         width: 200,
-         cellClick: openForm,
-         cssClass: 'RowClick',
-         headerTooltip: col => {
-            const identifier = 'purchase_orders_listings_table_status'
-            return tooltip(identifier)?.description || col.getDefinition().title
-         },
-      },
-      {
-         title: 'Item',
-         field: 'supplierItem',
-         headerFilter: false,
-         formatter: reactFormatter(<SupplierItemName />),
-         headerTooltip: col => {
-            const identifier = 'purchase_orders_listings_table_item_name'
-            return tooltip(identifier)?.description || col.getDefinition().title
-         },
-      },
-      {
-         title: 'Type',
-         field: 'packaging',
-         headerFilter: false,
-         formatter: reactFormatter(<LabelItem />),
-         width: 200,
-         headerTooltip: col => {
-            const identifier = 'purchase_orders_listings_table_order_type'
-            return tooltip(identifier)?.description || col.getDefinition().title
-         },
-      },
-   ]
-
-   if (loading) return <Loader />
 
    return (
       <>
@@ -131,42 +61,24 @@ export default function PurchaseOrders() {
                </Flex>
             </Flex>
             <Spacer size="16px" />
-            <ReactTabulator
-               ref={tableRef}
-               columns={columns}
-               data={purchaseOrderItems}
-               options={tableOptions}
+
+            <RadioGroup
+               options={[
+                  { id: 1, title: 'Supplier Items' },
+                  { id: 2, title: 'Packagings' },
+               ]}
+               active={1}
+               onChange={option => setView(option.title)}
             />
+
+            <Spacer size="16px" />
+
+            {view === 'Supplier Items' ? (
+               <ItemPurchaseOrders tableRef={tableRef} />
+            ) : (
+               <PackagingPurchaseOrders tableRef={tableRef} />
+            )}
          </Flex>
       </>
    )
-}
-
-function SupplierItemName({
-   cell: {
-      _cell: {
-         row: { data },
-      },
-   },
-}) {
-   if (data.supplierItem && data.supplierItem.name)
-      return data.supplierItem.name
-   if (data.packaging && data.packaging.packagingName)
-      return data.packaging.packagingName
-   return 'NA'
-}
-
-function LabelItem({
-   cell: {
-      _cell: {
-         row: { data },
-      },
-   },
-}) {
-   if (data && data.type === 'PACKAGING')
-      return <Tag color="success">Packaging</Tag>
-   if (data && data.type === 'SUPPLIER_ITEM')
-      return <Tag color="primary">Supplier Item</Tag>
-
-   return <Tag color="danger">NA</Tag>
 }
---
+++ src/apps/inventory/views/Listings/PurchaseOrders/supplierItem.jsx
@@ -0,0 +1,138 @@
+import { useSubscription } from '@apollo/react-hooks'
+import { ReactTabulator } from '@dailykit/react-tabulator'
+import {
+   Flex,
+   HorizontalTab,
+   HorizontalTabList,
+   HorizontalTabPanel,
+   HorizontalTabPanels,
+   HorizontalTabs,
+} from '@dailykit/ui'
+import React from 'react'
+import { toast } from 'react-toastify'
+import { v4 as uuid } from 'uuid'
+import { InlineLoader, Tooltip } from '../../../../../shared/components'
+import { useTooltip } from '../../../../../shared/providers'
+import { logger } from '../../../../../shared/utils'
+import { GENERAL_ERROR_MESSAGE } from '../../../constants/errorMessages'
+import { useTabs } from '../../../context'
+import { PURCHASE_ORDERS_SUBSCRIPTION } from '../../../graphql'
+import tableOptions from '../tableOption'
+
+export default function ItemPurchaseOrders({ tableRef }) {
+   const { addTab } = useTabs()
+   const { tooltip } = useTooltip()
+
+   const {
+      loading,
+      data: { purchaseOrderItems = [] } = {},
+      error,
+   } = useSubscription(PURCHASE_ORDERS_SUBSCRIPTION, {
+      variables: { type: 'SUPPLIER_ITEM' },
+   })
+
+   if (error) {
+      toast.error(GENERAL_ERROR_MESSAGE)
+      logger(error)
+   }
+
+   const openForm = (_, cell) => {
+      const { id } = cell.getData()
+      const tabTitle = `Purchase Order-${uuid().substring(30)}`
+      addTab(tabTitle, `/inventory/purchase-orders/item/${id}`)
+   }
+
+   const columns = [
+      {
+         title: 'Id',
+         field: 'id',
+         headerFilter: false,
+         cssClass: 'RowClick',
+         cellClick: openForm,
+      },
+      {
+         title: 'Item',
+         field: 'supplierItem.name',
+         headerFilter: false,
+         headerTooltip: col => {
+            const identifier = 'purchase_orders_listings_table_item_name'
+            return tooltip(identifier)?.description || col.getDefinition().title
+         },
+      },
+   ]
+
+   if (loading) return <InlineLoader />
+
+   return (
+      <HorizontalTabs>
+         <HorizontalTabList>
+            <HorizontalTab>
+               <Flex container alignItems="center">
+                  COMPLETED
+                  <Tooltip identifier="purchase-orders-listings_COMPLETED_tab" />
+               </Flex>
+            </HorizontalTab>
+            <HorizontalTab>
+               <Flex container alignItems="center">
+                  PENDING
+                  <Tooltip identifier="purchase-orders-listings_PENDING_tab" />
+               </Flex>
+            </HorizontalTab>
+            <HorizontalTab>
+               <Flex container alignItems="center">
+                  CANCELLED
+                  <Tooltip identifier="purchase-orders-listings_CANCELLED_tab" />
+               </Flex>
+            </HorizontalTab>
+            <HorizontalTab>
+               <Flex container alignItems="center">
+                  UNPUBLISHED
+                  <Tooltip identifier="purchase-orders-listings_UNPUBLISHED_tab" />
+               </Flex>
+            </HorizontalTab>
+         </HorizontalTabList>
+         <HorizontalTabPanels>
+            <HorizontalTabPanel>
+               <ReactTabulator
+                  ref={tableRef}
+                  columns={columns}
+                  data={purchaseOrderItems.filter(
+                     col => col.status === 'COMPLETED'
+                  )}
+                  options={tableOptions}
+               />
+            </HorizontalTabPanel>
+            <HorizontalTabPanel>
+               <ReactTabulator
+                  ref={tableRef}
+                  columns={columns}
+                  data={purchaseOrderItems.filter(
+                     col => col.status === 'PENDING'
+                  )}
+                  options={tableOptions}
+               />
+            </HorizontalTabPanel>
+            <HorizontalTabPanel>
+               <ReactTabulator
+                  ref={tableRef}
+                  columns={columns}
+                  data={purchaseOrderItems.filter(
+                     col => col.status === 'CANCELLED'
+                  )}
+                  options={tableOptions}
+               />
+            </HorizontalTabPanel>
+            <HorizontalTabPanel>
+               <ReactTabulator
+                  ref={tableRef}
+                  columns={columns}
+                  data={purchaseOrderItems.filter(
+                     col => col.status === 'UNPUBLISHED'
+                  )}
+                  options={tableOptions}
+               />
+            </HorizontalTabPanel>
+         </HorizontalTabPanels>
+      </HorizontalTabs>
+   )
+}
