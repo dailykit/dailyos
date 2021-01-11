e7e66ac57ddc96cd724e669eeea9a63fcc69f4e9
Foysal Ahammed <foysal.developer@gmail.com>
2021-01-11 15:32:06 +0600

    feat(responsiveness):make product apps customizeableProduct form responsive

--- src/apps/products/views/Forms/Product/CustomizableProduct/components/Pricing/styled.js
+++ src/apps/products/views/Forms/Product/CustomizableProduct/components/Pricing/styled.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
+   display: flex;
    background: #fff;
    padding: 18px;
+   margin-bottom: 16px;
+   section {
+      width: 100%;
+   }
+   @media screen and (max-width: 568px) {
+      flex-direction: column;
+      section {
+         margin-bottom: 8px;
+      }
+   }
 `
