9eceb9cb7eaea1e7b10d72cee61fa750d0874fdb
Praveen Bisht <prvnbist@gmail.com>
2021-01-18 20:32:12 +0530

    feat: app routes from database

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,56 @@
 import React from 'react'
-import { Trans } from 'react-i18next'
+import gql from 'graphql-tag'
 import Loadable from 'react-loadable'
 import { Loader } from '@dailykit/ui'
 import styled from 'styled-components'
+import { useSubscription } from '@apollo/react-hooks'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import { isKeycloakSupported } from './shared/utils'
 import { Lang, RedirectBanner } from './shared/components'
 
-const Safety = Loadable({
-   loader: () => import('./apps/safety'),
-   loading: Loader,
-})
-const Inventory = Loadable({
-   loader: () => import('./apps/inventory'),
-   loading: Loader,
-})
-const Products = Loadable({
-   loader: () => import('./apps/products'),
-   loading: Loader,
-})
-const Menu = Loadable({
-   loader: () => import('./apps/menu'),
-   loading: Loader,
-})
-const Settings = Loadable({
-   loader: () => import('./apps/settings'),
-   loading: Loader,
-})
-
-const Order = Loadable({
-   loader: () => import('./apps/order'),
-   loading: Loader,
-})
-
-const CRM = Loadable({
-   loader: () => import('./apps/crm'),
-   loading: Loader,
-})
-
-const Subscription = Loadable({
-   loader: () => import('./apps/subscription'),
-   loading: Loader,
-})
-
-const Insights = Loadable({
-   loader: () => import('./apps/insights'),
-   loading: Loader,
-})
-
-const Brands = Loadable({
-   loader: () => import('./apps/brands'),
-   loading: Loader,
-})
+const APPS = gql`
+   subscription apps {
+      apps(order_by: { id: asc }) {
+         id
+         title
+         icon
+         route
+      }
+   }
+`
 
 const App = () => {
+   const { loading, data: { apps = [] } = {} } = useSubscription(APPS)
+   if (loading) return <Loader />
    return (
       <>
          <Router basename={process.env.PUBLIC_URL}>
             <Switch>
                <Route path="/" exact>
                   <AppList>
-                     <AppItem>
-                        <Link to="/safety">
-                           <Trans i18nKey="safety">Safety</Trans>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/inventory">
-                           <Trans i18nKey="inventory">Inventory</Trans>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/products">
-                           <Trans i18nKey="products">Products</Trans>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/menu">
-                           <Trans i18nKey="menu">Menu</Trans>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/settings">
-                           <Trans i18nKey="settings">Settings</Trans>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/order">
-                           <Trans i18nKey="order">Order</Trans>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/crm">
-                           <Trans i18nKey="crm">CRM</Trans>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/subscription">
-                           <Trans i18nKey="subscription">Subscription</Trans>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/insights">
-                           <div>Insights</div>
-                        </Link>
-                     </AppItem>
-                     <AppItem>
-                        <Link to="/brands">
-                           <div>Brands</div>
-                        </Link>
-                     </AppItem>
+                     {apps.map(app => (
+                        <AppItem>
+                           <Link key={app.id} to={app.route}>
+                              {app.icon && (
+                                 <img src={app.icon} alt={app.title} />
+                              )}
+                              <span>{app.title}</span>
+                           </Link>
+                        </AppItem>
+                     ))}
                   </AppList>
                </Route>
-               <Route path="/inventory" component={Inventory} />
-               <Route path="/safety" component={Safety} />
-               <Route path="/products" component={Products} />
-               <Route path="/menu" component={Menu} />
-               <Route path="/settings" component={Settings} />
-               <Route path="/order" component={Order} />
-               <Route path="/crm" component={CRM} />
-               <Route path="/subscription" component={Subscription} />
-               <Route path="/insights" component={Insights} />
-               <Route path="/brands" component={Brands} />
+               {apps.map(app => (
+                  <Route
+                     key={app.id}
+                     path={app.route}
+                     component={Loadable({
+                        loading: Loader,
+                        loader: () => import(`./apps${app.route}`),
+                     })}
+                  />
+               ))}
             </Switch>
          </Router>
          {!isKeycloakSupported() && <RedirectBanner />}
@@ -136,8 +64,8 @@ export default App
 const AppList = styled.ul`
    display: grid;
    margin: 0 auto;
-   max-width: 980px;
    grid-gap: 16px;
+   max-width: 1180px;
    padding-top: 16px;
    width: calc(100vw - 40px);
    grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
@@ -151,14 +79,20 @@ const AppItem = styled.li`
       color: #2f256f;
       height: 100%;
       display: flex;
+      padding: 0 14px;
       border-radius: 2px;
       align-items: center;
-      justify-content: center;
       border: 1px solid #e0e0e0;
       text-decoration: none;
       transition: 0.4s ease-in-out;
       :hover {
          background: #f8f8f8;
+      }
+      img {
+         height: 32px;
+         width: 32px;
+         margin-right: 14px;
+         display: inline-block;
       }
    }
 `
