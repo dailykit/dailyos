05eeaf6aaf268e89aa3aed7277f92f7388930521
siddhantk232 <siddhantk232@gmail.com>
2021-01-21 13:58:20 +0530

    refactor: helper functions for anykit
    anykitHelpers.js stores fetch helpers and other
    helper functions required in useAnykitMatches hook

---
+++ src/apps/inventory/utils/anykitHelpers.js
@@ -0,0 +1,175 @@
+async function getSupplierItemMatches(
+   url,
+   supplierItemId,
+   headers,
+   controller
+) {
+   try {
+      const resp = await fetch(url, {
+         method: 'POST',
+         headers,
+         signal: controller.signal,
+         body: JSON.stringify({
+            query: `
+                  query GetIngredientSupplierItemMatches($supplierItemId: Int) {
+                     matches_ingredientSupplierItemMatch(
+                        where: {
+                           organizationSupplierItemId: { _eq: $supplierItemId }
+                        }
+                     ) {
+                        id
+                        ingredient {
+                           processings_aggregate {
+                              aggregate {
+                                 count
+                              }
+                              nodes {
+                                 name
+                                 sachets {
+                                    processing {
+                                       ingredient {
+                                          id
+                                          name
+                                       }
+                                    }
+                                    id
+                                    minQuantity
+                                    maxQuantity
+                                    unit
+                                    rawingredient_sachets {
+                                       rawIngredient {
+                                          id
+                                          data
+                                          recipe_ingredients {
+                                             recipe {
+                                                name
+                                                url
+                                             }
+                                          }
+                                       }
+                                    }
+                                 }
+                              }
+                           }
+                        }
+                     }
+                  }
+               `,
+            variables: { supplierItemId },
+         }),
+      }).then(r => r.json())
+
+      if (resp?.errors?.length) {
+         return [null, resp.errors[0]?.message || 'Unexpected error occured']
+      }
+
+      return [resp?.data?.matches_ingredientSupplierItemMatch || [], null]
+   } catch (e) {
+      return [null, e.message || 'Unexpected error occured']
+   }
+}
+
+async function getSachetItemMatches(
+   url,
+   sachetId,
+   supplierItemId,
+   headers,
+   controller
+) {
+   let sachetItemMatches = []
+   let err = null
+   const sachetSachetItemMatches = await fetch(url, {
+      method: 'POST',
+      headers,
+      signal: controller.signal,
+      body: JSON.stringify({
+         query: `
+                  query GetSachetSupplierItemMatches($sachetId: Int) {
+                     matches_sachetSachetItemMatch(
+                        where: { organizationSachetItemId: { _eq: $sachetId } }
+                     ) {
+                        id
+                        sachet {
+                           minQuantity
+                           maxQuantity
+                           rawingredient_sachets {
+                              rawIngredient {
+                                 id
+                                 data
+                              }
+                           }
+                           processing {
+                              name
+                              ingredient {
+                                 name
+                              }
+                           }
+                        }
+                     }
+                  }
+               `,
+         variables: { sachetId },
+      }),
+   }).then(r => r.json())
+
+   if (sachetSachetItemMatches?.errors?.length)
+      err =
+         sachetSachetItemMatches.errors[0].message || 'Unexpected error occured'
+
+   sachetItemMatches = [
+      ...(sachetSachetItemMatches?.data?.matches_sachetSachetItemMatch || []),
+      ...sachetItemMatches,
+   ]
+
+   const sachetSupplierItemMatches = await fetch(url, {
+      method: 'POST',
+      headers,
+      signal: controller.signal,
+      body: JSON.stringify({
+         query: `
+                  query GetSachetSupplierItemMatches($supplierItemId: Int) {
+                     matches_sachetSupplierItemMatch(
+                        where: {
+                           organizationSupplierItemId: { _eq: $supplierItemId }
+                        }
+                     ) {
+                        id
+                        sachet {
+                           minQuantity
+                           maxQuantity
+                           rawingredient_sachets {
+                              rawIngredient {
+                                 id
+                                 data
+                              }
+                           }
+                           processing {
+                              name
+                              ingredient {
+                                 name
+                              }
+                           }
+                        }
+                     }
+                  }
+               `,
+         variables: { supplierItemId },
+      }),
+   }).then(r => r.json())
+
+   if (sachetSupplierItemMatches?.errors?.length) {
+      err =
+         sachetSupplierItemMatches.errors[0].message ||
+         'Unexpected error occured'
+   } else {
+      sachetItemMatches = [
+         ...sachetItemMatches,
+         ...(sachetSupplierItemMatches?.data?.matches_sachetSupplierItemMatch ||
+            []),
+      ]
+   }
+
+   return [sachetItemMatches, err]
+}
+
+export default { getSachetItemMatches, getSupplierItemMatches }
--- src/apps/inventory/utils/useAnykitMatches.js
+++ src/apps/inventory/utils/useAnykitMatches.js
@@ -1,180 +1,8 @@
 import { useEffect, useState } from 'react'
 
+import helpers from './anykitHelpers'
+
 const BASE_URL = 'https://dailykit-recipe-hub.herokuapp.com/v1/graphql'
-
-async function getSupplierItemMatches(
-   url,
-   supplierItemId,
-   headers,
-   controller
-) {
-   try {
-      const resp = await fetch(url, {
-         method: 'POST',
-         headers,
-         signal: controller.signal,
-         body: JSON.stringify({
-            query: `
-                  query GetIngredientSupplierItemMatches($supplierItemId: Int) {
-                     matches_ingredientSupplierItemMatch(
-                        where: {
-                           organizationSupplierItemId: { _eq: $supplierItemId }
-                        }
-                     ) {
-                        id
-                        ingredient {
-                           processings_aggregate {
-                              aggregate {
-                                 count
-                              }
-                              nodes {
-                                 name
-                                 sachets {
-                                    processing {
-                                       ingredient {
-                                          id
-                                          name
-                                       }
-                                    }
-                                    id
-                                    minQuantity
-                                    maxQuantity
-                                    unit
-                                    rawingredient_sachets {
-                                       rawIngredient {
-                                          id
-                                          data
-                                          recipe_ingredients {
-                                             recipe {
-                                                name
-                                                url
-                                             }
-                                          }
-                                       }
-                                    }
-                                 }
-                              }
-                           }
-                        }
-                     }
-                  }
-               `,
-            variables: { supplierItemId },
-         }),
-      }).then(r => r.json())
-
-      if (resp?.errors?.length) {
-         return [null, resp.errors[0]?.message || 'Unexpected error occured']
-      }
-
-      return [resp?.data?.matches_ingredientSupplierItemMatch || [], null]
-   } catch (e) {
-      return [null, e.message || 'Unexpected error occured']
-   }
-}
-
-async function getSachetItemMatches(
-   url,
-   sachetId,
-   supplierItemId,
-   headers,
-   controller
-) {
-   let sachetItemMatches = []
-   let err = null
-   const sachetSachetItemMatches = await fetch(url, {
-      method: 'POST',
-      headers,
-      signal: controller.signal,
-      body: JSON.stringify({
-         query: `
-                  query GetSachetSupplierItemMatches($sachetId: Int) {
-                     matches_sachetSachetItemMatch(
-                        where: { organizationSachetItemId: { _eq: $sachetId } }
-                     ) {
-                        id
-                        sachet {
-                           minQuantity
-                           maxQuantity
-                           rawingredient_sachets {
-                              rawIngredient {
-                                 id
-                                 data
-                              }
-                           }
-                           processing {
-                              name
-                              ingredient {
-                                 name
-                              }
-                           }
-                        }
-                     }
-                  }
-               `,
-         variables: { sachetId },
-      }),
-   }).then(r => r.json())
-
-   if (sachetSachetItemMatches?.errors?.length)
-      err =
-         sachetSachetItemMatches.errors[0].message || 'Unexpected error occured'
-
-   sachetItemMatches = [
-      ...(sachetSachetItemMatches?.data?.matches_sachetSachetItemMatch || []),
-      ...sachetItemMatches,
-   ]
-
-   const sachetSupplierItemMatches = await fetch(url, {
-      method: 'POST',
-      headers,
-      signal: controller.signal,
-      body: JSON.stringify({
-         query: `
-                  query GetSachetSupplierItemMatches($supplierItemId: Int) {
-                     matches_sachetSupplierItemMatch(
-                        where: {
-                           organizationSupplierItemId: { _eq: $supplierItemId }
-                        }
-                     ) {
-                        id
-                        sachet {
-                           minQuantity
-                           maxQuantity
-                           rawingredient_sachets {
-                              rawIngredient {
-                                 id
-                                 data
-                              }
-                           }
-                           processing {
-                              name
-                              ingredient {
-                                 name
-                              }
-                           }
-                        }
-                     }
-                  }
-               `,
-         variables: { supplierItemId },
-      }),
-   }).then(r => r.json())
-
-   if (sachetSupplierItemMatches?.errors?.length) {
-      err =
-         sachetSupplierItemMatches.errors[0].message ||
-         'Unexpected error occured'
-   } else {
-      sachetItemMatches = [
-         ...sachetItemMatches,
-         ...(sachetSupplierItemMatches?.data?.matches_sachetSupplierItemMatch ||
-            []),
-      ]
-   }
-
-   return [sachetItemMatches, err]
-}
 
 export const useAnykitMatches = ({
    supplierItemId,
@@ -199,7 +27,7 @@ export const useAnykitMatches = ({
       setLoading(true)
 
       if (showSupplierItemMatches) {
-         const [matches, err] = await getSupplierItemMatches(
+         const [matches, err] = await helpers.getSupplierItemMatches(
             BASE_URL,
             supplierItemId,
             headers,
@@ -216,7 +44,7 @@ export const useAnykitMatches = ({
       }
 
       if (showSachetMatches) {
-         const [matches, err] = await getSachetItemMatches(
+         const [matches, err] = await helpers.getSachetItemMatches(
             BASE_URL,
             sachetId,
             supplierItemId,
