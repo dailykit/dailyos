ad9d76efc5741fd44b8c5a3a24a515b2ef96b64f
siddhantk232 <siddhantk232@gmail.com>
2021-01-21 13:27:44 +0530

    refactor: useAnykitMatches hook
    small helper function to fetch matches conditionally

--- src/apps/inventory/utils/useAnykitMatches.js
+++ src/apps/inventory/utils/useAnykitMatches.js
@@ -2,34 +2,19 @@ import { useEffect, useState } from 'react'
 
 const BASE_URL = 'https://dailykit-recipe-hub.herokuapp.com/v1/graphql'
 
-export const useAnykitMatches = ({
+async function getSupplierItemMatches(
+   url,
    supplierItemId,
-   sachetId,
-   showSachetMatches = false,
-}) => {
-   const [error, setError] = useState(null)
-   const [supplierItemMatches, setSupplierItemMatches] = useState([])
-   const [sachetItemMatches, setSatchetItemMatches] = useState([])
-   const [loading, setLoading] = useState(false)
-
-   const controller = new window.AbortController()
-
-   useEffect(() => {
-      const headers = {
-         'x-hasura-admin-secret': process.env.REACT_APP_ANYKIT_SECRET,
-         'content-type': 'application/json',
-      }
-
-      setLoading(true)
-
-      if (!showSachetMatches) {
-         // get matches from ingredientSupllierItemMatch table
-         fetch(BASE_URL, {
-            method: 'POST',
-            headers,
-            signal: controller.signal,
-            body: JSON.stringify({
-               query: `
+   headers,
+   controller
+) {
+   try {
+      const resp = await fetch(url, {
+         method: 'POST',
+         headers,
+         signal: controller.signal,
+         body: JSON.stringify({
+            query: `
                   query GetIngredientSupplierItemMatches($supplierItemId: Int) {
                      matches_ingredientSupplierItemMatch(
                         where: {
@@ -74,33 +59,35 @@ export const useAnykitMatches = ({
                      }
                   }
                `,
-               variables: { supplierItemId },
-            }),
-         })
-            .then(r => r.json())
-            .then(resp => {
-               setLoading(false)
-               if (resp?.errors) {
-                  // errors is always an array with atleast one item
-                  setError(resp.errors[0])
-               } else {
-                  setSupplierItemMatches(
-                     resp?.data?.matches_ingredientSupplierItemMatch || []
-                  )
-               }
-            })
-            .catch(err => {
-               setLoading(false)
-               setError(err)
-            })
-      } else {
-         // get matches from sachetSachetItem and sachetSupplierItemMatches table
-         fetch(BASE_URL, {
-            method: 'POST',
-            headers,
-            signal: controller.signal,
-            body: JSON.stringify({
-               query: `
+            variables: { supplierItemId },
+         }),
+      }).then(r => r.json())
+
+      if (resp?.errors?.length) {
+         return [null, resp.errors[0]?.message || 'Unexpected error occured']
+      }
+
+      return [resp?.data?.matches_ingredientSupplierItemMatch || [], null]
+   } catch (e) {
+      return [null, e.message || 'Unexpected error occured']
+   }
+}
+
+async function getSachetItemMatches(
+   url,
+   sachetId,
+   supplierItemId,
+   headers,
+   controller
+) {
+   let sachetItemMatches = []
+   let err = null
+   const sachetSachetItemMatches = await fetch(url, {
+      method: 'POST',
+      headers,
+      signal: controller.signal,
+      body: JSON.stringify({
+         query: `
                   query GetSachetSupplierItemMatches($sachetId: Int) {
                      matches_sachetSachetItemMatch(
                         where: { organizationSachetItemId: { _eq: $sachetId } }
@@ -125,32 +112,25 @@ export const useAnykitMatches = ({
                      }
                   }
                `,
-               variables: { sachetId },
-            }),
-         })
-            .then(r => r.json())
-            .then(resp => {
-               setLoading(false)
-               if (resp?.errors?.length) {
-                  setError(resp.errors[0])
-               } else {
-                  setSatchetItemMatches([
-                     ...(resp?.data?.matches_sachetSachetItemMatch || []),
-                     ...sachetItemMatches,
-                  ])
-               }
-            })
-            .catch(err => {
-               setLoading(false)
-               setError(err)
-            })
+         variables: { sachetId },
+      }),
+   }).then(r => r.json())
 
-         fetch(BASE_URL, {
-            method: 'POST',
-            headers,
-            signal: controller.signal,
-            body: JSON.stringify({
-               query: `
+   if (sachetSachetItemMatches?.errors?.length)
+      err =
+         sachetSachetItemMatches.errors[0].message || 'Unexpected error occured'
+
+   sachetItemMatches = [
+      ...(sachetSachetItemMatches?.data?.matches_sachetSachetItemMatch || []),
+      ...sachetItemMatches,
+   ]
+
+   const sachetSupplierItemMatches = await fetch(url, {
+      method: 'POST',
+      headers,
+      signal: controller.signal,
+      body: JSON.stringify({
+         query: `
                   query GetSachetSupplierItemMatches($supplierItemId: Int) {
                      matches_sachetSupplierItemMatch(
                         where: {
@@ -177,31 +157,87 @@ export const useAnykitMatches = ({
                      }
                   }
                `,
-               variables: { supplierItemId },
-            }),
-         })
-            .then(r => r.json())
-            .then(resp => {
-               setLoading(false)
-               if (resp?.errors?.length) {
-                  setError(resp.errors[0])
-               } else {
-                  setSatchetItemMatches([
-                     ...sachetItemMatches,
-                     ...(resp?.data?.matches_sachetSupplierItemMatch || []),
-                  ])
-               }
-            })
-            .catch(err => {
-               setLoading(false)
-               setError(err)
-            })
+         variables: { supplierItemId },
+      }),
+   }).then(r => r.json())
+
+   if (sachetSupplierItemMatches?.errors?.length) {
+      err =
+         sachetSupplierItemMatches.errors[0].message ||
+         'Unexpected error occured'
+   } else {
+      sachetItemMatches = [
+         ...sachetItemMatches,
+         ...(sachetSupplierItemMatches?.data?.matches_sachetSupplierItemMatch ||
+            []),
+      ]
+   }
+
+   return [sachetItemMatches, err]
+}
+
+export const useAnykitMatches = ({
+   supplierItemId,
+   sachetId,
+   showSachetMatches = false,
+   showSupplierItemMatches = true,
+   showIngredientSachetMatches = false,
+}) => {
+   const [error, setError] = useState(null)
+   const [supplierItemMatches, setSupplierItemMatches] = useState([])
+   const [sachetItemMatches, setSatchetItemMatches] = useState([])
+   const [loading, setLoading] = useState(false)
+
+   const controller = new window.AbortController()
+
+   const resolveData = async () => {
+      const headers = {
+         'x-hasura-admin-secret': process.env.REACT_APP_ANYKIT_SECRET,
+         'content-type': 'application/json',
       }
 
+      setLoading(true)
+
+      if (showSupplierItemMatches) {
+         const [matches, err] = await getSupplierItemMatches(
+            BASE_URL,
+            supplierItemId,
+            headers,
+            controller
+         )
+
+         setLoading(false)
+         if (err) {
+            setError(err)
+            return
+         }
+         setSupplierItemMatches(matches)
+         return
+      }
+
+      if (showSachetMatches) {
+         const [matches, err] = await getSachetItemMatches(
+            BASE_URL,
+            sachetId,
+            supplierItemId,
+            headers,
+            controller
+         )
+
+         setLoading(false)
+         if (err) {
+            setError(err)
+            return
+         }
+         setSatchetItemMatches(matches)
+         return
+      }
       return () => {
          controller.abort()
       }
-   }, [supplierItemId, sachetId, showSachetMatches])
+   }
+
+   useEffect(resolveData, [supplierItemId, sachetId, showSachetMatches])
 
    return {
       sachetItemMatches,
