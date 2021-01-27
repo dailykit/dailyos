b356d669206745310bf662e44cbabd989b35f735
Praveen Bisht <prvnbist@gmail.com>
2021-01-27 21:50:44 +0530

    feat(safety): use global sidebar and tabbar

--- src/apps/safety/index.jsx
+++ src/apps/safety/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
 import App from './App'
-import { TabProvider } from './context'
 import { AccessProvider, TooltipProvider } from '../../shared/providers'
 
 const SafetyApp = () => (
    <TooltipProvider app="Safety App">
       <AccessProvider app="Safety App">
-         <TabProvider>
-            <App />
-         </TabProvider>
+         <App />
       </AccessProvider>
    </TooltipProvider>
 )
