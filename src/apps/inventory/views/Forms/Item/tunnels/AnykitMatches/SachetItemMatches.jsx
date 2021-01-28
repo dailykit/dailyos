ead0f03119f49478e8d7ad8fed0180ea66e2ee07
siddhantk232 <siddhantk232@gmail.com>
2021-01-28 14:25:48 +0530

    feat: SachetItemMatches tunnel component

--- src/apps/inventory/views/Forms/Item/tunnels/AnykitMatches/SachetItemMatches.jsx
+++ src/apps/inventory/views/Forms/Item/tunnels/AnykitMatches/SachetItemMatches.jsx
@@ -1,12 +1,16 @@
 import {
-   Filler,
    Flex,
-   List,
-   ListItem,
+   SectionTab,
+   SectionTabList,
+   SectionTabPanel,
+   SectionTabPanels,
+   SectionTabs,
    Spacer,
+   Text,
    TunnelHeader,
 } from '@dailykit/ui'
 import React from 'react'
+
 import {
    ErrorState,
    InlineLoader,
@@ -15,20 +19,9 @@ import {
 import { logger } from '../../../../../../../shared/utils'
 import { useAnykitMatches } from '../../../../../utils/useAnykitMatches'
 import { TunnelBody } from '../styled'
+import { RecipeSource } from './RecipeSource'
 
 export default function SachetItemMatches({ close, sachetItemId }) {
-   const { error, sachetItemMatches, loading } = useAnykitMatches({
-      sachetId: sachetItemId,
-      showSachetMatches: true,
-   })
-
-   if (error) {
-      logger(error)
-      return <ErrorState />
-   }
-
-   if (loading) return <InlineLoader />
-
    return (
       <>
          <TunnelHeader
@@ -41,26 +34,76 @@ export default function SachetItemMatches({ close, sachetItemId }) {
          />
 
          <TunnelBody>
-            {sachetItemMatches.length ? (
-               <List>
-                  {sachetItemMatches.map(match => {
-                     console.log(match)
-                     return (
-                        <ListItem
-                           key={match.id}
-                           type="SSL2"
-                           content={{
-                              title: '',
-                              description: '',
-                           }}
-                        />
-                     )
-                  })}
-               </List>
-            ) : (
-               <Filler message="No matches found" />
-            )}
+            <SachetSachetItemMatches sachetItemId={sachetItemId} />
          </TunnelBody>
       </>
+   )
+}
+
+function SachetSachetItemMatches({ sachetItemId }) {
+   const { error, sachetItemMatches, loading } = useAnykitMatches({
+      sachetId: sachetItemId,
+      showSachetItemMatches: true,
+   })
+
+   if (error) {
+      logger(error)
+      return <ErrorState />
+   }
+
+   if (loading) return <InlineLoader />
+
+   if (!sachetItemMatches.length)
+      return <ErrorState message="No matches found!" />
+
+   return (
+      <SectionTabs>
+         <SectionTabList>
+            {sachetItemMatches.map(match => (
+               <SectionTab key={match.id}>
+                  <Flex padding="14px" style={{ textAlign: 'left' }}>
+                     {/* refactor it into a function for handling null values */}
+                     {match.sachet.processing.ingredient.name},{' '}
+                     {match.sachet.processing.name},{' '}
+                     {match.sachet.minQuantity || match.sachet.maxQuantity}{' '}
+                     {match.sachet.unit || 'unit'}
+                  </Flex>
+               </SectionTab>
+            ))}
+         </SectionTabList>
+         <SectionTabPanels>
+            {sachetItemMatches.map(match => (
+               <SectionTabPanel key={match.id}>
+                  <SectionTabs>
+                     <SectionTabList>
+                        <Text as="subtitle">Parsed from</Text>
+                        <Spacer size="8px" />
+                        {match.sachet.rawingredient_sachets.map(rs => (
+                           <SectionTab key={rs.rawIngredient.id}>
+                              <Flex
+                                 padding="14px"
+                                 style={{ textAlign: 'left' }}
+                              >
+                                 {rs.rawIngredient.data}
+                              </Flex>
+                           </SectionTab>
+                        ))}
+                     </SectionTabList>
+                     <SectionTabPanels>
+                        {match.sachet.rawingredient_sachets.map(rs => (
+                           <SectionTabPanel key={rs.rawIngredient.id}>
+                              <Text as="h3">Used in Recipes</Text>
+                              <Spacer size="8px" />
+                              <RecipeSource
+                                 rawIngredientId={rs.rawIngredient.id}
+                              />
+                           </SectionTabPanel>
+                        ))}
+                     </SectionTabPanels>
+                  </SectionTabs>
+               </SectionTabPanel>
+            ))}
+         </SectionTabPanels>
+      </SectionTabs>
    )
 }
