e4425b209076a732a8980a7e799a76a6c3f9afff
Praveen Bisht <prvnbist@gmail.com>
2021-04-08 18:30:39 +0530

    fix: unscrollable

--- src/apps/editor/components/FileExplorer/styles.js
+++ src/apps/editor/components/FileExplorer/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const FileExplorerWrapper = styled.div`
    width: 100%;
-   height: 100vh;
+   height: calc(100vh - 231px);
    max-height: 85vh;
    padding-bottom: 16px;
    overflow: auto;
