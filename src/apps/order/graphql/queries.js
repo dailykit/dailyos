9b02ad768eab137d5dc300c4a212bb1e648caece
Praveen Bisht <prvnbist@gmail.com>
2021-04-12 15:50:55 +0530

    fix: query write error

--- src/apps/order/graphql/queries.js
+++ src/apps/order/graphql/queries.js
@@ -122,6 +122,7 @@ export const QUERIES = {
                displayImage
                operationConfigId
                operationConfig {
+                  id
                   labelTemplateId
                   stationId
                }
