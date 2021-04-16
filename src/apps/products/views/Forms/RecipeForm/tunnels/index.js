212e628b9d0ff16748ee4a83195d6a669088713c
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-04-16 13:39:52 +0530

    feat: create product from recipe

--- src/apps/products/views/Forms/RecipeForm/tunnels/index.js
+++ src/apps/products/views/Forms/RecipeForm/tunnels/index.js
@@ -7,6 +7,7 @@ import PhotoTunnel from './PhotoTunnel'
 import CardPreviewTunnel from './CardPreviewTunnel'
 import StepPhotoTunnel from './StepPhotoTunnel'
 import YieldInfoTunnel from './YieldInfoTunnel'
+import CreateProductTunnel from './CreateProductTunnel'
 
 export {
    InformationTunnel,
@@ -18,4 +19,5 @@ export {
    CardPreviewTunnel,
    StepPhotoTunnel,
    YieldInfoTunnel,
+   CreateProductTunnel,
 }
