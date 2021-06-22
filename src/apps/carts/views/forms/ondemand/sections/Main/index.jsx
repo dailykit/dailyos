5b34b24343f4413c84040c2152dda1186036d7a2
nitinrajsingh1997 <45280052+nitinrajsingh1997@users.noreply.github.com>
2021-06-22 11:46:58 +0530

    feat/manual-order/all-products (#633)
    * feat: added search for products
    
    * refactor: based on PR review
    
    * feat: added separate component for cards
    
    * fix: debouncing and race conditions
    
    * refactor: based on PR review
    
    * refactor: moved debounce from hooks to utils
    
    * refactor: removed renderPrice
    
    Co-authored-by: Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>

--- src/apps/carts/views/forms/ondemand/sections/Main/index.jsx
+++ src/apps/carts/views/forms/ondemand/sections/Main/index.jsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import moment from 'moment'
-import { isEqual } from 'lodash'
+import { forEach, isEqual } from 'lodash'
 import styled, { css } from 'styled-components'
 import { toast } from 'react-toastify'
 import { useParams } from 'react-router'
 import { Element } from 'react-scroll'
 import { useLazyQuery, useMutation, useQuery } from '@apollo/react-hooks'
+import debounce from '../../../../../../../shared/utils/debounce'
 import {
    AnchorNav,
    AnchorNavItem,
    Text,
    Flex,
    Filler,
-   TextButton,
    Spacer,
+   RadioGroup,
+   IconButton,
+   SearchBox,
 } from '@dailykit/ui'
 
 import { useManual } from '../../state'
-import { buildImageUrl } from '../../../../../utils'
 import { MUTATIONS, QUERIES } from '../../../../../graphql'
 import EmptyIllo from '../../../../../assets/svgs/EmptyIllo'
-import { InlineLoader } from '../../../../../../../shared/components'
-import { currencyFmt, logger } from '../../../../../../../shared/utils'
+import {
+   InlineLoader,
+   ProductCards,
+} from '../../../../../../../shared/components'
+import {
+   calcDiscountedPrice,
+   currencyFmt,
+   logger,
+} from '../../../../../../../shared/utils'
+import { SearchIcon, CloseIcon } from '../../../../../../../shared/assets/icons'
 
 export const Main = () => {
    const { brand } = useManual()
@@ -30,6 +40,17 @@ export const Main = () => {
    const [isMenuEmpty, setIsMenuEmpty] = React.useState(false)
    const [hasMenuError, setHasMenuError] = React.useState(false)
    const [isMenuLoading, setIsMenuLoading] = React.useState(true)
+
+   const [showMenu, setShowMenu] = React.useState(true)
+   const [search, setSearch] = React.useState('')
+   const [showSearch, setShowSearch] = React.useState(false)
+   const [menuProductIds, setMenuProductsIds] = React.useState([])
+
+   const options = [
+      { id: 1, title: 'Menu Store' },
+      { id: 2, title: 'All Products' },
+   ]
+
    const [fetchProducts] = useLazyQuery(QUERIES.PRODUCTS.LIST, {
       onCompleted: ({ products = [] }) => {
          const _menu = []
@@ -41,6 +62,8 @@ export const Main = () => {
                ),
             })
          })
+         const productIds = products.map(product => product.id)
+         setMenuProductsIds(productIds)
          setMenu(_menu)
          setIsMenuEmpty(false)
          setHasMenuError(false)
@@ -151,164 +174,288 @@ export const Main = () => {
       )
    return (
       <Styles.Main>
-         <Menu menu={menu} />
+         <Flex
+            container
+            width="100%"
+            alignItems="center"
+            justifyContent="space-between"
+         >
+            <Flex container alignItems="center" justifyContent="flex-start">
+               <RadioGroup
+                  options={options}
+                  active={1}
+                  onChange={() => setShowMenu(!showMenu)}
+               />
+            </Flex>
+
+            {showSearch && (
+               <SearchBox
+                  width="100%"
+                  onBlur={() => setShowSearch(false)}
+                  placeholder="Search"
+                  value={search}
+                  hasReadAccess={true}
+                  hasWriteAccess={true}
+                  fallBackMessage="You shall not pass!"
+                  onChange={e => setSearch(e.target.value)}
+               />
+            )}
+         </Flex>
+         <Spacer size="20px" />
+         {showMenu ? (
+            <Menu menu={menu} menuProductIds={menuProductIds} />
+         ) : (
+            <AllProducts />
+         )}
       </Styles.Main>
    )
 }
 
-const Menu = ({ menu }) => {
+const Menu = ({ menu, menuProductIds }) => {
    const { id: cartId } = useParams()
    const { cart, tunnels, dispatch } = useManual()
 
-   const [insertCartItem, { loading }] = useMutation(
-      MUTATIONS.CART.ITEM.INSERT,
-      {
-         onCompleted: () => {
-            toast.success('Item added to cart!')
-         },
-         onError: error => {
-            logger(error)
-            toast.error('Failed to add product to cart!')
-         },
-      }
+   const [showSearch, setShowSearch] = React.useState(false)
+   const [searchedResult, setSearchedResult] = React.useState([])
+   const [isLoading, setIsLoading] = React.useState(false)
+   const [input, setInput] = React.useState('')
+
+   const [searchProducts] = useLazyQuery(QUERIES.PRODUCTS.LIST, {
+      onCompleted: data => {
+         setIsLoading(false)
+         setSearchedResult(data.products)
+      },
+      fetchPolicy: 'network-only',
+   })
+
+   return (
+      <>
+         {showSearch ? (
+            <Flex
+               container
+               width="100%"
+               alignItems="center"
+               justifyContent="space-between"
+            >
+               <SearchBox
+                  placeholder="Search"
+                  value={input}
+                  hasReadAccess={true}
+                  hasWriteAccess={true}
+                  fallBackMessage="You shall not pass!"
+                  onChange={e => {
+                     setInput(e.target.value)
+                     searchProducts({
+                        variables: {
+                           where: {
+                              id: { _in: menuProductIds },
+                              name: { _ilike: `%${e.target.value}%` },
+                           },
+                        },
+                     })
+                  }}
+               />
+
+               <IconButton
+                  type="ghost"
+                  size="sm"
+                  onClick={() => setShowSearch(false)}
+               >
+                  <CloseIcon color="#ec3333" />
+               </IconButton>
+            </Flex>
+         ) : (
+            <Flex
+               container
+               width="100%"
+               alignItems="center"
+               justifyContent="space-between"
+            >
+               <AnchorNav>
+                  {menu.map(item => (
+                     <AnchorNavItem
+                        key={item.title}
+                        label={item.title}
+                        targetElement={item.title}
+                        containerId="categories"
+                     />
+                  ))}
+               </AnchorNav>
+
+               <IconButton
+                  type="ghost"
+                  size="sm"
+                  onClick={() => setShowSearch(true)}
+               >
+                  <SearchIcon color="#888D9D" />
+               </IconButton>
+            </Flex>
+         )}
+         <Spacer size="10px" />
+         {showSearch ? (
+            <ProductCards
+               input={input}
+               isLoading={isLoading}
+               data={searchedResult}
+               cart={cart}
+            />
+         ) : (
+            <Element
+               id="categories"
+               style={{
+                  width: '100%',
+                  overflowY: 'auto',
+                  overflowX: 'hidden',
+                  position: 'relative',
+                  height: '95.7%',
+                  padding: '0 14px',
+               }}
+            >
+               {menu.map(item => (
+                  <Element
+                     key={item.title}
+                     name={item.title}
+                     style={{ height: '100%', overflowY: 'auto' }}
+                  >
+                     <Text as="text1">{item.title}</Text>
+                     <Spacer size="14px" />
+                     <ProductCards data={item.products} cart={cart} />
+
+                     <Spacer size="24px" />
+                  </Element>
+               ))}
+            </Element>
+         )}
+      </>
+   )
+}
+
+const AllProducts = () => {
+   const { id: cartId } = useParams()
+   const { cart, tunnels, dispatch } = useManual()
+   const [showSearch, setShowSearch] = React.useState(false)
+   const [input, setInput] = React.useState('')
+   const [allProducts, setAllProducts] = React.useState([])
+   const [searchedResult, setSearchedResult] = React.useState([])
+   const [searchProducts, { loading }] = useLazyQuery(QUERIES.PRODUCTS.LIST, {
+      onCompleted: data => {
+         if (input) {
+            // hack for race condition
+            setSearchedResult(data.products)
+         }
+      },
+      fetchPolicy: 'network-only',
+   })
+
+   useQuery(QUERIES.PRODUCTS.LIST, {
+      variables: {
+         where: { isPublished: { _eq: true }, isArchived: { _eq: false } },
+      },
+      onCompleted: data => {
+         setAllProducts(data.products)
+      },
+   })
+
+   const optimizedSearchProducts = React.useCallback(
+      debounce(searchProducts, 500),
+      []
    )
 
-   const calcDiscountedPrice = (price, discount) => {
-      return price - price * (discount / 100)
-   }
-
-   const renderPrice = product => {
-      if (product.isPopupAllowed) {
-         if (product.discount) {
-            return (
-               <Flex container alignItems="center">
-                  <Styles.Price strike>
-                     {currencyFmt(product.price)}
-                  </Styles.Price>{' '}
-                  <Styles.Price>
-                     {currencyFmt(
-                        calcDiscountedPrice(product.price, product.discount)
-                     )}
-                  </Styles.Price>
-               </Flex>
-            )
-         }
-         return <Styles.Price>{currencyFmt(product.price)}</Styles.Price>
-      } else {
-         const totalPrice =
-            product.defaultCartItem.unitPrice +
-            product.defaultCartItem.childs?.data?.reduce(
-               (acc, op) => acc + op.unitPrice,
-               0
-            )
-
-         return <Styles.Price>{currencyFmt(totalPrice)}</Styles.Price>
-      }
-   }
-
-   const handleAddProduct = (e, categoryTitle) => {
-      const { productId } = e.target.dataset
-      if (productId && !loading) {
-         const category = menu.find(item => item.title === categoryTitle)
-         const product = category.products.find(pdct => pdct.id === +productId)
-
-         if (product.isPopupAllowed) {
-            dispatch({
-               type: 'SET_PRODUCT_ID',
-               payload: productId,
-            })
-            switch (product.type) {
-               case 'simple':
-                  return tunnels.productOptions[1](1)
-               case 'customizable':
-                  return tunnels.customizableComponents[1](1)
-               case 'combo':
-                  return tunnels.comboComponents[1](1)
-            }
-         } else {
-            insertCartItem({
-               variables: {
-                  object: {
-                     ...product.defaultCartItem,
-                     cartId: +cartId,
-                  },
+   const handleSearch = value => {
+      setInput(value)
+      if (value.trim()) {
+         optimizedSearchProducts({
+            variables: {
+               where: {
+                  isPublished: { _eq: true },
+                  isArchived: { _eq: false },
+                  name: { _ilike: `%${value}%` },
                },
-            })
-         }
+            },
+         })
+      } else {
+         setSearchedResult([])
       }
    }
 
    return (
       <>
-         <AnchorNav>
-            {menu.map(item => (
-               <AnchorNavItem
-                  key={item.title}
-                  label={item.title}
-                  targetElement={item.title}
-                  containerId="categories"
+         {showSearch ? (
+            <Flex
+               container
+               width="100%"
+               alignItems="center"
+               justifyContent="space-between"
+            >
+               <SearchBox
+                  placeholder="Search"
+                  value={input}
+                  hasReadAccess={true}
+                  hasWriteAccess={true}
+                  fallBackMessage="You shall not pass!"
+                  onChange={e => handleSearch(e.target.value)}
                />
-            ))}
-         </AnchorNav>
-         <Element
-            id="categories"
-            style={{
-               width: '100%',
-               overflowY: 'auto',
-               overflowX: 'hidden',
-               position: 'relative',
-               height: '95.7%',
-               padding: '0 14px',
-            }}
-         >
-            {menu.map(item => (
-               <Element
-                  key={item.title}
-                  name={item.title}
-                  style={{ height: '100%', overflowY: 'auto' }}
-                  onClick={e => handleAddProduct(e, item.title)}
+               <IconButton
+                  type="ghost"
+                  size="sm"
+                  onClick={() => setShowSearch(false)}
                >
-                  <Text as="text1">{item.title}</Text>
-                  <Spacer size="14px" />
-                  <Styles.Cards>
-                     {item.products.map(product => (
-                        <Styles.Card key={product.id}>
-                           <aside>
-                              {product.assets?.images &&
-                              product.assets?.images?.length > 0 ? (
-                                 <img
-                                    alt={product.name}
-                                    src={buildImageUrl(
-                                       '56x56',
-                                       product.assets?.images[0]
-                                    )}
-                                 />
-                              ) : (
-                                 <span>N/A</span>
-                              )}
-                           </aside>
-                           <Flex as="main" container flexDirection="column">
-                              <Text as="text2">{product.name}</Text>
-                              <Text as="text3">{renderPrice(product)}</Text>
-                              <Spacer size="8px" />
-                              {cart?.paymentStatus === 'PENDING' && (
-                                 <TextButton
-                                    type="solid"
-                                    variant="secondary"
-                                    size="sm"
-                                    data-product-id={product.id}
-                                 >
-                                    ADD {product.isPopupAllowed && '+'}
-                                 </TextButton>
-                              )}
-                           </Flex>
-                        </Styles.Card>
-                     ))}
-                  </Styles.Cards>
-                  <Spacer size="24px" />
+                  <CloseIcon color="#ec3333" />
+               </IconButton>
+            </Flex>
+         ) : (
+            <Flex
+               container
+               width="100%"
+               alignItems="center"
+               justifyContent="space-between"
+            >
+               <AnchorNav>
+                  <AnchorNavItem
+                     targetElement="element-1"
+                     label="All Products"
+                     containerId="containerElement"
+                  />
+               </AnchorNav>
+
+               <IconButton
+                  type="ghost"
+                  size="sm"
+                  onClick={() => setShowSearch(true)}
+               >
+                  <SearchIcon color="#888D9D" />
+               </IconButton>
+            </Flex>
+         )}
+         <Spacer size="10px" />
+         {showSearch ? (
+            <ProductCards
+               input={input}
+               isLoading={loading}
+               data={searchedResult}
+               cart={cart}
+            />
+         ) : (
+            <Element
+               id="containerElement"
+               style={{
+                  position: 'relative',
+                  height: '600px',
+                  overflowY: 'auto',
+                  overflowX: 'hidden',
+                  width: '100%',
+               }}
+            >
+               <Element
+                  name="element-1"
+                  style={{
+                     height: '1000px',
+                  }}
+               >
+                  <ProductCards data={allProducts} cart={cart} />
                </Element>
-            ))}
-         </Element>
+            </Element>
+         )}
       </>
    )
 }
@@ -317,50 +464,13 @@ const Styles = {
    Main: styled.main`
       grid-area: main;
       overflow-y: auto;
+      padding: 10px;
    `,
-   Cards: styled.ul`
-      display: grid;
-      grid-gap: 14px;
-      grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
-   `,
-   Card: styled.li`
-      padding: 4px;
-      display: grid;
-      grid-gap: 8px;
-      min-height: 56px;
-      border-radius: 2px;
-      background: #ffffff;
-      border: 1px solid #ececec;
-      grid-template-columns: auto 1fr;
-      aside {
-         width: 56px;
-         height: 56px;
-         display: flex;
-         background: #eaeaea;
-         align-items: center;
-         justify-content: center;
-         > span {
-            font-size: 14px;
-            color: #ab9e9e;
-         }
-         > img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            border-radius: 2px;
-         }
-      }
-   `,
+
    Filler: styled(Filler)`
       p {
          font-size: 14px;
          text-align: center;
       }
    `,
-   Price: styled.p(
-      ({ strike }) => css`
-         text-decoration-line: ${strike ? 'line-through' : 'none'};
-         margin-right: ${strike ? '1ch' : '0'};
-      `
-   ),
 }
